ecda041214d0e1ca21bdc87ed74feb3c1b79e96b
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-15 22:57:20 +0300

    Fix navigation title span class name

--- bookreads/client/src/components/Header/Navigation/Navigation.js
+++ bookreads/client/src/components/Header/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = () => {
     return (
         <nav className="navigation header__navigation">
             <h2 className="navigation__title navigation__title--bookreads">
-                book<span className="navigation__title--reads">reads</span>
+                book<span className="navigation__span navigation__span--reads">reads</span>
             </h2>
         </nav>
     );
