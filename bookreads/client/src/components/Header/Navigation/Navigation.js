73bb3084506ef57a82dc742563a3e640530fdec3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-15 22:28:48 +0300

    Create header nav items

--- bookreads/client/src/components/Header/Navigation/Navigation.js
+++ bookreads/client/src/components/Header/Navigation/Navigation.js
@@ -1,7 +1,9 @@
 const Navigation = () => {
     return (
         <nav className="navigation header__navigation">
-
+            <h2 className="navigation__title navigation__title--bookreads">
+                book<span className="navigation__title--reads">reads</span>
+            </h2>
         </nav>
     );
 }
