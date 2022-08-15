eec7d57c6cab1ded3c7a737cbfa54ef4edc552ff
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-15 22:23:11 +0300

    Import header navigation component

--- bookreads/client/src/components/Header/Header.js
+++ bookreads/client/src/components/Header/Header.js
@@ -1,7 +1,9 @@
+import Navigation from "./Navigation/Navigation";
+
 const Header = () => {
     return (
         <header className="header">
-
+            <Navigation />
         </header>
     );
 }
