64723f10f7c42bae57bac01bcebf2f51a5a414ee
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 22:04:04 +0300

    Fix auth component form

--- bookreads/client/src/components/Auth/Auth.js
+++ bookreads/client/src/components/Auth/Auth.js
@@ -2,22 +2,22 @@ import { Link } from 'react-router-dom';
 
 import Logo from '../Logo/Logo';
 
+import './Auth.css';
+
 const Auth = () => {
     return (
-        <main>
+        <main className="main__auth">
             <Logo />
-            <form>
-                <legend></legend>
+            <form className="form__auth">
+                <legend className="form__auth--legend">Sign In</legend>
 
-                <label />
-                <input />
+                <label htmlFor="email" className="form__auth--label">Email</label>
+                <input type="text" className="form__auth--input" />
 
-                <label />
-                <input />
+                <label htmlFor="password" className="form__auth--label">Password</label>
+                <input type="password" className="form__auth--input" />
 
-                <button>
-
-                </button>
+                <button className="form__auth--button">Sign In</button>
             </form>
         </main>
     );
