364fa440666d51af36916ceebf112692dd5fb817
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 22:59:48 +0300

    Add section content info

--- bookreads/client/src/components/Auth/Auth.js
+++ bookreads/client/src/components/Auth/Auth.js
@@ -17,8 +17,15 @@ const Auth = () => {
                 <label htmlFor="password" className="form__auth--label">Password</label>
                 <input type="password" className="form__auth--input" />
 
-                <button className="form__auth--button">Sign In</button>
+                <button className="form__auth--button auth__button">Sign In</button>
             </form>
+            <section className="auth__section section__content">
+                <p className="section__content--paragraph">
+                    By signing in, you agree to the Bookreads Terms of Service and Privacy Policy.
+                </p>
+                <span className="section__content--span">Don't have an account?</span>
+                <button className="section__content--button auth__button">Sign Up</button>
+            </section>
         </main>
     );
 }
