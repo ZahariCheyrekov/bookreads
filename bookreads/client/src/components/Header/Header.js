e83a4c96dc4d46a300aae2380c5a8509314b8588
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 11:04:45 +0300

    Fix application title

--- bookreads/client/src/components/Header/Header.js
+++ bookreads/client/src/components/Header/Header.js
@@ -1,11 +1,17 @@
 import Navigation from "./Navigation/Navigation";
 
 import './Header.css';
+import books from '../../assets/books.png'
 
 const Header = () => {
     return (
         <header className="header">
             <Navigation />
+            <section className="header__section">
+                <article className="section__article section__article--books" >
+                    <img className="article__books article__books--img" src={books} alt="books" />
+                </article>
+            </section>
         </header>
     );
 }
