f5fec5dec49cc2a3e7018ac2c44083a21df6809e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 14:38:25 +0300

    Create home component

---
+++ bookreads/client/src/components/Home/Home.js
@@ -0,0 +1,33 @@
+import './Home.css';
+
+const Home = () => {
+    return (
+        <main className="main">
+            <div className="main__wrapper">
+                <section className="wrapper__section wrapper__section--about">
+                    <article className="section__article section__article--about">
+                        <h4 className="article__title">
+                            Deciding what to read next?
+                        </h4>
+                        <p className="article__paragraph">
+                            You're in the right place. Tell us what titles or genres
+                            you've enjoyed in the past, and we'll give you surprisingly
+                            insightful recommendations.
+                        </p>
+                    </article>
+                    <article className="section__article section__article--about">
+                        <h4 className="article__title">
+                            What are your friends reading?
+                        </h4>
+                        <p className="article__paragraph">
+                            Chances are your friends are discussing their favorite
+                            (and least favorite) books on Bookreads.
+                        </p>
+                    </article>
+                </section>
+            </div>
+        </main>
+    );
+}
+
+export default Home;
