21424ae0c9cc039712f87501187ea0089143b8ff
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 21:20:13 +0300

    Create auth component

---
+++ bookreads/client/src/components/Auth/Auth.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import Logo from '../Logo/Logo';
+
+const Auth = () => {
+    return (
+        <main>
+            <Logo />
+            <form>
+                <legend></legend>
+
+                <label />
+                <input />
+
+                <label />
+                <input />
+
+                <button>
+
+                </button>
+            </form>
+        </main>
+    );
+}
+
+export default Auth;
