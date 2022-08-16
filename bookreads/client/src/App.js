3b47a33ecb6ec91651e1f5a3e6c17854e51305a4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 15:11:02 +0300

    Import footer component

--- bookreads/client/src/App.js
+++ bookreads/client/src/App.js
@@ -3,12 +3,14 @@ import './index.css';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <>
             <Header />
             <Home />
+            <Footer />
         </>
     );
 }
