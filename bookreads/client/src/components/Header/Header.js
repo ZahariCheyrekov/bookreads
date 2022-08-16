9c1e7434da49d912dc09a73ef24c0037490b8e82
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 17:26:14 +0300

    Create header aside

--- bookreads/client/src/components/Header/Header.js
+++ bookreads/client/src/components/Header/Header.js
@@ -1,9 +1,13 @@
-import Navigation from "./Navigation/Navigation";
+import { Link, useNavigate } from 'react-router-dom';
 
-import './Header.css';
+import Navigation from "./Navigation/Navigation";
 import books from '../../assets/books-banner.png'
 
+import './Header.css';
+
 const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <header className="header">
             <Navigation />
@@ -17,12 +21,28 @@ const Header = () => {
                         <h2 className="article__title--big">
                             Tell the world what you are reading.
                         </h2>
+
+                        <aside className="header__aside">
+                            <h3 className="header__aside--title">
+                                Discover & read more
+                            </h3>
+                            <button className="aside__button--email" onClick={() => navigate('/nopage')}>
+                                Sign up with email
+                            </button>
+                            <p>
+                                Already a member?
+                                &nbsp;
+                                <Link to={'Sign In'}>
+                                    Sign In
+                                </Link>
+                            </p>
+                        </aside>
                     </article>
 
                     <img className="article__books article__books--img" src={books} alt="books" />
                 </article>
             </section>
-        </header>
+        </header >
     );
 }
 
