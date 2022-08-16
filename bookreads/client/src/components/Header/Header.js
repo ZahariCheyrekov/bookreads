fb5510848c097b22009fc9b1c9963d495675b1c4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 11:44:31 +0300

    Fix header info

--- bookreads/client/src/components/Header/Header.js
+++ bookreads/client/src/components/Header/Header.js
@@ -7,8 +7,18 @@ const Header = () => {
     return (
         <header className="header">
             <Navigation />
+
             <section className="header__section">
-                <article className="section__article section__article--books" >
+                <article className="section__article" >
+                    <article className="article__article article__article--info">
+                        <h3 className="article__title--small">
+                            A place to find your next book.
+                        </h3>
+                        <h2 className="article__title--big">
+                            Tell the world what you are reading.
+                        </h2>
+                    </article>
+
                     <img className="article__books article__books--img" src={books} alt="books" />
                 </article>
             </section>
