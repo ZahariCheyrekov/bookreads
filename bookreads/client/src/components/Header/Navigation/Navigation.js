6e975ff340416704d29ed7e22f7d4a22cd6ae242
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-16 21:19:21 +0300

    Import logo component

--- bookreads/client/src/components/Header/Navigation/Navigation.js
+++ bookreads/client/src/components/Header/Navigation/Navigation.js
@@ -1,17 +1,11 @@
-import { Link } from 'react-router-dom';
-import bookreads from '../../../assets/bookreads-logo.png'
+import Logo from '../../Logo/Logo';
 
 import './Navigation.css';
 
 const Navigation = () => {
     return (
         <nav className="navigation header__navigation">
-         
-            <article className="header__navigation--article">
-                <Link to={'/'}>
-                    <img src={bookreads} className="navigation__article--logo" alt="bookreads" />
-                </Link>
-            </article>
+            <Logo />
         </nav>
     );
 }
