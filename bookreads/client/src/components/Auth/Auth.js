1503c9b580dcf5754f688ac221c471c78f87ee65
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-17 10:19:57 +0300

    Add auth component use effect and fix form

--- bookreads/client/src/components/Auth/Auth.js
+++ bookreads/client/src/components/Auth/Auth.js
@@ -1,30 +1,66 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import Logo from '../Logo/Logo';
 
 import './Auth.css';
 
 const Auth = () => {
+    const { pathname } = useLocation();
+    const [isSignIn, setIsSignIn] = useState();
+
+    useEffect(() => {
+        if (pathname === '/signin') {
+            setIsSignIn(true);
+        } else if (pathname === '/signup') {
+            setIsSignIn(false);
+        }
+    }, [pathname]);
+
     return (
         <main className="main__auth">
             <Logo />
             <form className="form__auth">
-                <legend className="form__auth--legend">Sign In</legend>
+                <legend className="form__auth--legend">
+                    {isSignIn ? "Sign In" : "Sign Up"}
+                </legend>
+
+                {!isSignIn && (
+                    <>
+                        <label htmlFor="name" className="form__auth--label">Your name</label>
+                        <input type="text" name="name" className="form__auth--input" />
+                    </>
+                )}
 
                 <label htmlFor="email" className="form__auth--label">Email</label>
                 <input type="text" className="form__auth--input" />
 
                 <label htmlFor="password" className="form__auth--label">Password</label>
-                <input type="password" className="form__auth--input" />
+                <input type="password" name="password" className="form__auth--input" />
 
-                <button className="form__auth--button auth__button">Sign In</button>
+                {!isSignIn && (
+                    <>
+                        <label htmlFor="repeatPassword" className="form__auth--label">Repeat password</label>
+                        <input type="text" name="repeatPassword" className="form__auth--input" />
+                    </>
+                )}
+
+                <button className="form__auth--button auth__button">
+                    {isSignIn ? "Sign In" : "Sign Up"}
+                </button>
             </form>
             <section className="auth__section section__content">
                 <p className="section__content--paragraph">
                     By signing in, you agree to the Bookreads Terms of Service and Privacy Policy.
                 </p>
-                <span className="section__content--span">Don't have an account?</span>
-                <button className="section__content--button auth__button">Sign Up</button>
+                <span className="section__content--span">
+                    {isSignIn ? "Don't have an account?" : "Already have an account?"}
+                </span>
+                <Link to={isSignIn ? "/signup" : "/signin"}>
+                    <button className="section__content--button auth__button">
+                        {isSignIn ? "Sign Up" : "Sign In"}
+                    </button>
+                </Link>
             </section>
         </main>
     );
