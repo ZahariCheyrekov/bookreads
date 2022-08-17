c3253e3f9ea29eaad9cbd5cab7c01440560e5ec1
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-17 18:34:44 +0300

    Add header list

--- bookreads/client/src/components/Navigation/Navigation.js
+++ bookreads/client/src/components/Navigation/Navigation.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Logo from '../Logo/Logo';
 
 import './Navigation.css';
@@ -6,6 +7,25 @@ const Navigation = () => {
     return (
         <nav className="navigation header__navigation">
             <Logo />
+            <ul className="header__ul">
+                <li className="header__ul--li">
+                    <Link to={'/home'}>
+                        Home
+                    </Link>
+                </li>
+                <li className="header__ul--li">
+                    <Link to={'/mybooks'} >
+                        My Books
+                    </Link>
+                </li>
+                <li className="header__ul--li">
+                    Browse
+                </li>
+            </ul>
+
+            <form className="header__form--search">
+                <input className="header__form--input" placeholder="Search books" />
+            </form>
         </nav>
     );
 }
