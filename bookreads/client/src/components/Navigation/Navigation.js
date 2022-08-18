b4b3df48768086a86ebbf971138a1b8c2bf33155
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-18 19:40:38 +0300

    Get user from the app state

--- bookreads/client/src/components/Navigation/Navigation.js
+++ bookreads/client/src/components/Navigation/Navigation.js
@@ -1,32 +1,41 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Logo from '../Logo/Logo';
 
 import './Navigation.css';
 
 const Navigation = () => {
+    const user = useSelector((state) => state.user);
+    console.log(user.user);
+
     return (
         <nav className="navigation header__navigation">
             <Logo />
-            <ul className="header__ul">
-                <li className="header__ul--li">
-                    <Link to={'/home'}>
-                        Home
-                    </Link>
-                </li>
-                <li className="header__ul--li">
-                    <Link to={'/mybooks'} >
-                        My Books
-                    </Link>
-                </li>
-                <li className="header__ul--li">
-                    Browse
-                </li>
-            </ul>
+            {user?.user?.email ?
+                <>
+                    <ul className="header__ul">
+                        <li className="header__ul--li">
+                            <Link to={'/home'}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="header__ul--li">
+                            <Link to={'/mybooks'} >
+                                My Books
+                            </Link>
+                        </li>
+                        <li className="header__ul--li">
+                            Browse
+                        </li>
+                    </ul>
 
-            <form className="header__form--search">
-                <input className="header__form--input" placeholder="Search books" />
-            </form>
+                    <form className="header__form--search">
+                        <input className="header__form--input" placeholder="Search books" />
+                    </form>
+                </>
+                : null
+            }
         </nav>
     );
 }
