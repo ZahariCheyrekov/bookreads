b0ec31eae5b227d30a0f708321768eaa339f3bfa
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-18 19:01:22 +0300

    Import auth context provider

--- bookreads/client/src/index.js
+++ bookreads/client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
 
-// import store from '../src/app/store';
+import { AuthContextProvider } from './contexts/AuthContext';
 
 import App from './App';
 
@@ -11,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            {/* <Provider store={store}> */}
+            <AuthContextProvider>
                 <App />
-            {/* </Provider> */}
+            </AuthContextProvider>
         </BrowserRouter>
     </React.StrictMode >
 );
