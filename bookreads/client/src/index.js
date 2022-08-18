2eaaeae1122b668a6a97e171e349bdecef415631
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-18 19:20:42 +0300

    Fix index.js

--- bookreads/client/src/index.js
+++ bookreads/client/src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { AuthContextProvider } from './contexts/AuthContext';
-
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <AuthContextProvider>
-                <App />
-            </AuthContextProvider>
+            <App />
         </BrowserRouter>
     </React.StrictMode >
 );
