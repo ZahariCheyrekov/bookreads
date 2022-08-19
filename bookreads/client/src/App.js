0a4384cef3cff2c1763d8ae73b783ee7268ab037
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-19 12:10:29 +0300

    Import navigation and user profile component

--- bookreads/client/src/App.js
+++ bookreads/client/src/App.js
@@ -7,16 +7,20 @@ import Welcome from './components/Welcome/Welcome';
 import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer';
 import Auth from './components/Auth/Auth';
+import Profile from './components/Profile/Profile';
 import NotFound from './components/NotFound/NotFound';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
     return (
         <>
+            <Navigation />
             <Routes>
                 <Route path="/" element={<Welcome />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/signin" element={<Auth />} />
                 <Route path="/signup" element={<Auth />} />
+                <Route path="/user/:name/:id" element={<Profile />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
