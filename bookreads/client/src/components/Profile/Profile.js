e82d86b71fc45d472e3a3833764dab94e497cf27
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-19 12:35:14 +0300

    Implement profile component content

--- bookreads/client/src/components/Profile/Profile.js
+++ bookreads/client/src/components/Profile/Profile.js
@@ -1,8 +1,30 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import defaultUserPhoto from '../../assets/default-user-photo.png';
+
+import './Profile.css';
+
 const Profile = () => {
+    const { user } = useSelector((state) => state.user);
+
     return (
-        <>
-            Profile
-        </>
+        <main className="main__profile">
+            <section className="main__profile--section">
+                <article className="profile__section--article">
+                    <img src={user?.imageUrl ? user.imageUrl : defaultUserPhoto}
+                        alt={`${user.name}`}
+                    />
+                </article>
+                <h4 className="profile__section--user">
+                    {user.name}
+                </h4>
+                <Link to={'/user/edit'}>
+                    <button className="profile__section--edit">
+                        Edit profile
+                    </button>
+                </Link>
+            </section>
+        </main>
     );
 }
 
