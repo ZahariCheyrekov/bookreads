c6eb0a07e235b3402d36acc3d5c77202e0545ea8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-19 15:51:57 +0300

    Add api authorization header

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 import { BASE_URL } from '../constants/url';
+import { getToken, getUser } from '../services/localStorage';
 
 const API = axios.create({ baseURL: BASE_URL });
 
-// API.interceptors.request.use((req) => {
-//     const user = getUser();
+API.interceptors.request.use((req) => {
+    const user = getUser();
 
-//     if (user) {
-//         const userToken = getToken();
-//         req.headers.authorization = `Bearer ${userToken}`;
-//     }
-//     return req;
-// });
+    if (user) {
+        const userToken = getToken();
+        req.headers.authorization = `Bearer ${userToken}`;
+    }
+    return req;
+});
 
 export const signin = (userData) => API.post(`/user/signin`, userData);
 export const signup = (userData) => API.post(`/user/signup`, userData);
