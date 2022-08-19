e11d27c4fb5e17998bb5f4e288ac28913ff1f73f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-19 14:53:04 +0300

    Add user routes

--- bookreads/server/server.js
+++ bookreads/server/server.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 import { configDatabase } from './config/configDatabase.js';
 import { DEFAULT_PORT } from './constants/index.js';
 
+import userRoutes from './routes/user.js';
+
 const app = express();
 dotenv.config();
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors());
+
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Application is running correctly.');
