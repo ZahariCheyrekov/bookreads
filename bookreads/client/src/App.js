c6d03de55d0f5c1c12c03e64e532d943f29456e8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-19 22:51:15 +0300

    Import protected routes component

--- bookreads/client/src/App.js
+++ bookreads/client/src/App.js
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import './index.css';
 
+import Navigation from './components/Navigation/Navigation';
+import Auth from './components/Auth/Auth';
 import Welcome from './components/Welcome/Welcome';
 import Home from './components/Home/Home';
-import Footer from './components/Footer/Footer';
-import Auth from './components/Auth/Auth';
 import Profile from './components/Profile/Profile';
 import NotFound from './components/NotFound/NotFound';
-import Navigation from './components/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
 
 function App() {
     return (
@@ -17,10 +18,12 @@ function App() {
             <Navigation />
             <Routes>
                 <Route path="/" element={<Welcome />} />
-                <Route path="/home" element={<Home />} />
                 <Route path="/user/signin" element={<Auth />} />
                 <Route path="/user/signup" element={<Auth />} />
-                <Route path="/user/:name/:id" element={<Profile />} />
+                <Route element={<ProtectedRoutes />}>
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/user/:name/:id" element={<Profile />} />
+                </Route>
                 <Route path="*" element={<NotFound />} />
             </Routes>
             <Footer />
