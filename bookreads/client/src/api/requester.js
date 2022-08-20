86fcf9fa9d9feda33792220ba75bc4b9a7ba7fa3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 19:40:44 +0300

    Create get book by id api request

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -15,7 +15,8 @@ API.interceptors.request.use((req) => {
     return req;
 });
 
-export const createBook = (bookData) => API.post(`/books`, bookData);
+export const getBookById = (id) => API.get(`/books/${id}`);
+export const createBook = (bookData) => API.post('/books', bookData);
 
 export const signin = (userData) => API.post(`/user/signin`, userData);
 export const signup = (userData) => API.post(`/user/signup`, userData);
