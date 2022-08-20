4059f2f50ecff8d30a19e6a747790658d101385f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 21:50:49 +0300

    Add book details summary section

--- bookreads/client/src/components/Books/BookDetails/BookDetails.js
+++ bookreads/client/src/components/Books/BookDetails/BookDetails.js
@@ -1,37 +1,47 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getBookById } from '../../../api/requester';
+
 import { getCard } from '../../../services/book';
+
 import './BookDetails.css';
 
 const BookDetails = () => {
     const { id } = useParams();
-    const [book, setBook] = useState({});
-
-    const fetchBook = async () => {
-        const book = await getCard(id);
-        setBook(book);
-    }
-
-    console.log(book);
+    const [book, setBook] = useState(null);
 
     useEffect(() => {
+        const fetchBook = async () => {
+            const book = await getCard(id);
+            setBook(book);
+        }
         fetchBook();
-    }, []);
+    }, [id]);
 
     return (
         <main className="main__auth">
-            <div className="div__wrapper">
-                <aside className="aside__book--content">
-                    <article className="aside__book--article">
-                        <img src={book.bookCoverUrl} alt={book.title} />
-                    </article>
-                </aside>
-                <section>
+            {book ?
+                <div className="div__wrapper" >
+                    <aside className="aside__book--content">
+                        <article className="aside__book--article">
+                            <img src={book.bookCoverUrl} alt={book.title} />
+                        </article>
+                    </aside>
+                    <section className="section__book--content">
+                        <h1 className="book__title">{book.title}</h1>
+                        <h2 className="book__author">{book.author}</h2>
 
-                </section>
-            </div>
-        </main>
+                        <summary className="book__summary">
+                            {book.description.map((paragraph, index) =>
+                                <p key={index} className="book__summary--paragraph">
+                                    {paragraph}
+                                </p>
+                            )}
+                        </summary>
+                    </section>
+                </div >
+                : null
+            }
+        </main >
     );
 }
 
