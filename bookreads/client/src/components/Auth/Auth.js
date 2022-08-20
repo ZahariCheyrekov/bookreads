d91f21075e6bfda70278331eb676e2d84c436e9e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 16:53:43 +0300

    Fix auth component classes

--- bookreads/client/src/components/Auth/Auth.js
+++ bookreads/client/src/components/Auth/Auth.js
@@ -41,31 +41,31 @@ const Auth = () => {
     return (
         <main className="main__auth">
             <Logo />
-            <form className="form__auth">
-                <legend className="form__auth--legend">{isSignIn ? "Sign In" : "Sign Up"}</legend>
+            <form className="main__form">
+                <legend className="form__legend">{isSignIn ? "Sign In" : "Sign Up"}</legend>
 
                 {!isSignIn && (
                     <>
-                        <label htmlFor="name" className="form__auth--label">Your name</label>
-                        <input type="text" name="name" className="form__auth--input" onChange={handleChange} />
+                        <label htmlFor="name" className="form__label">Your name</label>
+                        <input type="text" name="name" className="form__input" onChange={handleChange} />
                     </>
                 )}
 
-                <label htmlFor="email" className="form__auth--label">Email</label>
-                <input type="text" name="email" className="form__auth--input" onChange={handleChange} />
+                <label htmlFor="email" className="form__label">Email</label>
+                <input type="text" name="email" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="password" className="form__auth--label password">Password
-                    <input type={showPassword ? "text" : "password"} name="password" className="form__auth--input password" onChange={handleChange} />
+                <label htmlFor="password" className="form__label password">Password
+                    <input type={showPassword ? "text" : "password"} name="password" className="form__input password" onChange={handleChange} />
                     <i className="fa-regular fa-eye" onClick={handleClick}></i>
                 </label>
 
                 {!isSignIn && (
                     <>
-                        <label htmlFor="repeatPassword" className="form__auth--label password">Repeat password</label>
-                        <input type={showPassword ? "text" : "password"} name="repeatPassword" className="form__auth--input password" onChange={handleChange} />
+                        <label htmlFor="repeatPassword" className="form__label password">Repeat password</label>
+                        <input type={showPassword ? "text" : "password"} name="repeatPassword" className="form__input password" onChange={handleChange} />
                     </>
                 )}
-                <button className="form__auth--button auth__button" onClick={handleAuth}>
+                <button className="form__button auth__button" onClick={handleAuth}>
                     {isSignIn ? "Sign In" : "Sign Up"}
                 </button>
             </form>
