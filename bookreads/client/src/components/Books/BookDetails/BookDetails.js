919a487f2df925b2e54f05e106a63bfacbca9417
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 19:40:55 +0300

    Create book state

--- bookreads/client/src/components/Books/BookDetails/BookDetails.js
+++ bookreads/client/src/components/Books/BookDetails/BookDetails.js
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getBookById } from '../../../api/requester';
+import { getCard } from '../../../services/book';
 import './BookDetails.css';
 
 const BookDetails = () => {
+    const { id } = useParams();
+    const [book, setBook] = useState({});
+
+    const fetchBook = async () => {
+        const book = await getCard(id);
+        setBook(book);
+    }
+
+    console.log(book);
+
+    useEffect(() => {
+        fetchBook();
+    }, []);
+
     return (
-        <>
-            BookDetails
-        </>
+        <main className="main__auth">
+            <div className="div__wrapper">
+                <aside className="aside__book--content">
+                    <article className="aside__book--article">
+                        <img src={book.bookCoverUrl} alt={book.title} />
+                    </article>
+                </aside>
+                <section>
+
+                </section>
+            </div>
+        </main>
     );
 }
 
