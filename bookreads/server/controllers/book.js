0e1f3469890a0f6b926afc23f22c56fc3d34b86c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 19:39:55 +0300

    Implement get book by id controller function

--- bookreads/server/controllers/book.js
+++ bookreads/server/controllers/book.js
@@ -1,11 +1,20 @@
-import mongoose from 'mongoose';
-
 import BookSchema from '../models/Book.js';
+
+export const getBookById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const book = await BookSchema.findById(id);
+        res.status(200).json(book);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
 
 export const createBook = async (req, res) => {
     const bookData = req.body;
     bookData.description = bookData.description.split('\n ');
-    
+
     const newBook = new BookSchema(bookData);
 
     try {
