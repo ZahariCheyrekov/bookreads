9a752d5ae6bcd22ffcb69120065c4412eaa0db8a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-20 16:54:24 +0300

    Create book form component

--- bookreads/client/src/components/Books/BookForm/BookForm.js
+++ bookreads/client/src/components/Books/BookForm/BookForm.js
@@ -1,42 +1,66 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { createBook } from "../../../api/requester";
+import { AuthContext } from "../../../contexts/AuthContext";
 
 import './BookForm.css';
 
 const BookForm = () => {
-    const [bookData, setBookData] = useState({ title: '', author: '', description: '', tags: '', pages: '', datePublished: '' });
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+    const [bookData, setBookData] = useState(
+        {
+            title: '',
+            author: '',
+            description: '',
+            tags: '',
+            pages: '',
+            language: '',
+            bookCoverUrl: '',
+            datePublished: ''
+        }
+    );
 
     const handleChange = (ev) => {
         setBookData({ ...bookData, [ev.target.name]: ev.target.value });
     }
-
     const handleClick = (ev) => {
         ev.preventDefault();
+
+        createBook({ ...bookData, creatorToken: user?.token });
+        navigate('/home');
     }
 
     return (
         <main className="main__auth">
             <form className="main__form--book">
-                <legend className="form__book--legend">CreateBook</legend>
+                <legend className="form__legend">Create book</legend>
 
-                <label htmlFor="title" className="form__book--label">Title</label>
-                <input type="text" name="title" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="title" className="form__label">Title</label>
+                <input type="text" name="title" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="author" className="form__book--label">Author</label>
-                <input type="text" name="author" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="author" className="form__label">Author</label>
+                <input type="text" name="author" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="description" className="form__book--label">Description</label>
-                <input type="text" name="description" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="description" className="form__label">Description</label>
+                <input type="text" name="description" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="tags" className="form__book--label">Tags</label>
-                <input type="text" name="tags" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="tags" className="form__label">Tags</label>
+                <input type="text" name="tags" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="pages" className="form__book--label">Pages</label>
-                <input type="number" name="pages" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="pages" className="form__label">Pages</label>
+                <input type="number" name="pages" className="form__input" onChange={handleChange} />
 
-                <label htmlFor="datePusblished" className="form__book--label">Date published</label>
-                <input type="date" name="datePusblished" className="form__book--input" onChange={handleChange} />
+                <label htmlFor="language" className="form__label">Language</label>
+                <input type="text" name="language" className="form__input" onChange={handleChange} />
 
-                <button className="form__book--button" onClick={handleClick}>Create book</button>
+                <label htmlFor="bookCoverUrl" className="form__label">Book cover url</label>
+                <input type="text" name="bookCoverUrl" className="form__input" onChange={handleChange} />
+
+                <label htmlFor="datePublished" className="form__label">Date published</label>
+                <input type="date" name="datePublished" className="form__input" onChange={handleChange} />
+
+                <button className="form__button auth__button" onClick={handleClick}>Create book</button>
             </form>
         </main >
     );
