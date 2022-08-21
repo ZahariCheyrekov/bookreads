a39fc204ed9ae9c25b522e0c893174bdecf8ed40
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-21 22:10:16 +0300

    Create book table component

---
+++ bookreads/client/src/components/Books/BookDetails/BookTable/BookTable.js
@@ -0,0 +1,39 @@
+import './BookTable.css';
+
+const BookTable = ({ book }) => {
+    return (
+        <table className="book__table">
+            <tbody className="book__table--tbody">
+                <tr className="book__table--row">
+                    <th className="book__table--heading">This edition</th>
+                </tr>
+                <tr className="book__table--row">
+                    <td className="book__table--data">
+                        Pages
+                    </td>
+                    <td className="book__table--data">
+                        {book.pages}
+                    </td>
+                </tr>
+                <tr className="book__table--row">
+                    <td className="book__table--data">
+                        Language
+                    </td>
+                    <td className="book__table--data">
+                        {book.language}
+                    </td>
+                </tr>
+                <tr className="book__table--row">
+                    <td className="book__table--data">
+                        Published
+                    </td>
+                    <td className="book__table--data">
+                        {new Date(book.datePublished).toDateString()}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+export default BookTable;
