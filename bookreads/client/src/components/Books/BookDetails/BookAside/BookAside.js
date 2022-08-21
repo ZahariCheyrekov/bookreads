8c90a7caa99fe0058bf38d1d16321c9a49b886bd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-21 22:09:49 +0300

    Create book aside component

---
+++ bookreads/client/src/components/Books/BookDetails/BookAside/BookAside.js
@@ -0,0 +1,34 @@
+import { Link, useNavigate } from 'react-router-dom';
+
+import { deleteBook } from '../../../../api/requester';
+
+const BookAside = ({ id, isOwner, bookCoverUrl, title }) => {
+    const navigate = useNavigate();
+
+    const handleDelete = () => {
+        deleteBook(id);
+        navigate('/');
+    }
+
+    return (
+        <aside className="aside__book--content">
+            <article className="aside__book--article">
+                <img src={bookCoverUrl} alt={title} />
+            </article>
+            {isOwner && (
+                <>
+                    <Link to={`/books/${id}/edit`}>
+                        <button className="aside__book--button">
+                            Edit book
+                        </button>
+                    </Link>
+                    <button className="aside__book--button book__delete--button" onClick={handleDelete}>
+                        Delete book
+                    </button>
+                </>
+            )}
+        </aside>
+    );
+}
+
+export default BookAside;
