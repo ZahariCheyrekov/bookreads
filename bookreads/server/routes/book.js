26bf7a91fa30dedb083327472670f18b323422dd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-21 16:42:31 +0300

    Import edit book function and add edit book route

--- bookreads/server/routes/book.js
+++ bookreads/server/routes/book.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getBookById, createBook } from '../controllers/book.js';
+import { getBookById, createBook, editBook } from '../controllers/book.js';
 
 const router = express.Router();
 
 router.get('/:id', getBookById);
 router.post('/', createBook);
+router.patch('/:id/edit', editBook);
 
 export default router;
