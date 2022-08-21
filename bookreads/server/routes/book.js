cf94fcff28479a360ce8fb3de288ab35bbb76464
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-21 21:46:55 +0300

    Add delete book function and create delete route

--- bookreads/server/routes/book.js
+++ bookreads/server/routes/book.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getBookById, createBook, editBook } from '../controllers/book.js';
+import { getBookById, createBook, editBook, deleteBook } from '../controllers/book.js';
 
 const router = express.Router();
 
 router.get('/:id', getBookById);
 router.post('/', createBook);
 router.patch('/:id/edit', editBook);
+router.delete('/:id', deleteBook);
 
 export default router;
