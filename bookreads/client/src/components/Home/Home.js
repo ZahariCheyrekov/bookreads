63b628c92fda6b18a201b0112574a1838362990d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-22 11:06:00 +0300

    Implement home component conditional rendering

--- bookreads/client/src/components/Home/Home.js
+++ bookreads/client/src/components/Home/Home.js
@@ -1,7 +1,18 @@
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import Welcome from '../Welcome/Welcome';
+
 const Home = () => {
+    const { user } = useContext(AuthContext);
+
     return (
         <>
-            Home
+            {user
+                ? <div>
+                    Home
+                </div>
+                : <Welcome />
+            }
         </>
     );
 }
