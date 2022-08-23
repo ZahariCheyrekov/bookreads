ed780579f9df2e72d0d6e7f93e6b32a8d655d598
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 23:07:23 +0300

    Import posts component style

--- bookreads/client/src/components/Home/Posts/Posts.js
+++ bookreads/client/src/components/Home/Posts/Posts.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import { getPosts } from '../../../services/post';
 import Post from './Post/Post';
 
+import './Posts.css';
+
 const Posts = () => {
     const [posts, setPosts] = useState([]);
 
@@ -13,6 +15,7 @@ const Posts = () => {
         }
         fetchPosts();
     }, []);
+    console.log(posts);
 
     return (
         <section className="posts">
