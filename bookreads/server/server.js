63e081bb06f13b9bf24a87ecf90eb22bab908d6d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 17:19:15 +0300

    Import post routes

--- bookreads/server/server.js
+++ bookreads/server/server.js
@@ -8,6 +8,7 @@ import { DEFAULT_PORT } from './constants/index.js';
 
 import userRoutes from './routes/user.js';
 import bookRoutes from './routes/book.js';
+import postRoutes from './routes/post.js';
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(cors());
 
 app.use('/user', userRoutes);
 app.use('/books', bookRoutes);
+app.use('/', postRoutes);
 
 app.get('/', (req, res) => {
     res.send('Application is running correctly.');
