057881589f6e0c45c6ff512e1f54c92b25605238
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 11:47:21 +0300

    Fix rating component classes

--- bookreads/client/src/components/Books/Details/Rating/Rating.js
+++ bookreads/client/src/components/Books/Details/Rating/Rating.js
@@ -4,27 +4,38 @@ import './Rating.css';
 
 const stars = [1, 2, 3, 4, 5];
 
-const Rating = () => {
+const Rating = ({ setParentRating, showRateTitle, small }) => {
     const [rating, setRating] = useState(null);
     const [hoverStar, setHoverStar] = useState(null);
 
     return (
-        <article className="rating">
+        <article className={`rating ${small && 'small'}`}>
             <span className="rating__stars">
                 {stars.map(star =>
                     <i
-                        className={`fa-solid fa-star ${star - 1 < (hoverStar || rating) ? 'rated' : 'unrated'}`}
+                        className=
+                        {`fa-solid fa-star 
+                        ${star - 1 < (hoverStar || rating) ? 'rated' : 'unrated'} 
+                        ${small && 'small'}`
+                        }
                         key={star}
-                        onClick={() => setRating(star)}
+                        onClick={() => {
+                            setRating(star);
+                            setParentRating(star);
+                        }}
                         onMouseEnter={() => setHoverStar(star)}
                         onMouseLeave={() => setHoverStar(null)}
                     />
                 )}
             </span>
-            <h4 className="rating__title">
-                Rate this book
-            </h4>
-        </article>
+            {
+                showRateTitle !== false
+                    ? <h4 className="rating__title">
+                        Rate this book
+                    </h4>
+                    : ''
+            }
+        </article >
     );
 }
 
