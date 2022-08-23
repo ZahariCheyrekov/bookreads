cb6f90d638a338062641ea86212e4f1467f88b20
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 17:19:52 +0300

    Implement create post controller function

---
+++ bookreads/server/controllers/post.js
@@ -0,0 +1,15 @@
+import PostSchema from '../models/Post.js';
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostSchema(post);
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
+}
