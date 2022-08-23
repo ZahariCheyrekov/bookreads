d0c8c1628cad07d7c9f1cf493d0bb789c4208f5f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 17:41:44 +0300

    Create posts component

---
+++ bookreads/client/src/components/Home/Posts/Posts.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import { getPosts } from '../../../services/post';
+import Post from './Post/Post';
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const posts = await getPosts();
+            setPosts(posts);
+        }
+        fetchPosts();
+    }, []);
+
+    return (
+        <section className="posts">
+            {posts.length > 0 ?
+                posts.map(post =>
+                    <Post
+                        key={post._id}
+                        post={post}
+                    />
+                )
+                : null}
+        </section>
+    );
+}
+
+export default Posts;
