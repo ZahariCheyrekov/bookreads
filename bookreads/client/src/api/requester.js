390f9bbf4409e577cb31269262654fb30e0ed385
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-23 23:06:47 +0300

    Add get user by id request function

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -23,5 +23,7 @@ export const deleteBook = (bookId) => API.delete(`/books/${bookId}`);
 export const getPosts = () => API.get('/');
 export const createPost = (postData) => API.post('/', postData);
 
+export const getUserById = (userId) => API.get(`/user/${userId}`);
+
 export const signin = (userData) => API.post(`/user/signin`, userData);
 export const signup = (userData) => API.post(`/user/signup`, userData);
