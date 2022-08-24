65c088c000a48d6e2c02ae9540c84530f3c42cb5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-24 18:14:32 +0300

    Import like post function and add route for it

--- bookreads/server/routes/post.js
+++ bookreads/server/routes/post.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts, createPost } from '../controllers/post.js';
+import { getPosts, createPost, likePost } from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.post('/posts/:id', likePost)
 
 export default router;
