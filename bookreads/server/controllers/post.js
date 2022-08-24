d2de2636e8161f3bd336cc7d7256601e08f5d8b0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-24 18:15:02 +0300

    Create like post controller and import mongoose

--- bookreads/server/controllers/post.js
+++ bookreads/server/controllers/post.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 import PostSchema from '../models/Post.js';
 
 export const getPosts = async (req, res) => {
@@ -23,4 +25,26 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`No posts with id: ${id}`);
+    }
+
+    const post = await PostSchema.findById(id);
+
+    const index = post.likes.findIndex((currentId) => currentId === String(id));
+
+    if (index === -1) {
+        post.likes.push(id);
+    } else {
+        post.likes = post.likes.filter((currentId) => currentId !== String(id));
+    }
+
+    const updatedPost = await PostSchema.findByIdAndUpdate(id, post, { new: true });
+
+    res.json(updatedPost.likes);
 }
