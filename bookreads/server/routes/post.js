27b62abdb07e97f25ce4d40b7b53c3ba88655032
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-25 18:01:03 +0300

    Add create comment function and build route for it

--- bookreads/server/routes/post.js
+++ bookreads/server/routes/post.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts, createPost, likePost } from '../controllers/post.js';
+import { getPosts, createPost, likePost, createComment } from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
-router.post('/posts/:id', likePost)
+router.post('/posts/:id', likePost);
+router.post('/post/:id/comment', createComment);
 
 export default router;
