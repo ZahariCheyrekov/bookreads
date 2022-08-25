46d088d3cbcbe33fa50b502219822e630645a8f5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-25 21:53:45 +0300

    Import get comments function and add route for it

--- bookreads/server/routes/post.js
+++ bookreads/server/routes/post.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { getPosts, createPost, likePost, createComment } from '../controllers/post.js';
+import { getPosts, createPost, likePost, createComment, getComments } from '../controllers/post.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.post('/posts/:id', likePost);
+router.get('/post/:id/comments', getComments);
 router.post('/post/:id/comment', createComment);
 
 export default router;
