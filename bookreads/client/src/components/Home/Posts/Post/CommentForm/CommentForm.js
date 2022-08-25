9aacc8c569a166831554d95f010eb32a5be6e993
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-25 18:32:58 +0300

    Create handle change and  fix textarea and button

--- bookreads/client/src/components/Home/Posts/Post/CommentForm/CommentForm.js
+++ bookreads/client/src/components/Home/Posts/Post/CommentForm/CommentForm.js
@@ -7,19 +7,46 @@ import './CommentForm.css';
 
 const CommentForm = () => {
     const { user } = useContext(AuthContext);
+    const [comment, setComment] = useState('');
+    const [commentButtonDisabled, setCommentButtonDisabled] = useState(true);
     const [showCommentButton, setShowCommentButton] = useState(false);
 
     const handleClick = () => {
         setShowCommentButton(true);
     }
 
+    const handleChange = (ev) => {
+        setComment(ev.target.value);
+
+        if (ev.target.value.trim() !== '') {
+            setCommentButtonDisabled(false);
+        } else {
+            setCommentButtonDisabled(true);
+        }
+    }
+
+    const handleComment = () => {
+
+    }
+
     return (
         <article className="post__article--comment">
             <User user={user?.result?.name} creatorId={user?.result?._id} />
             <form className="post__form">
-                <textarea className="post__form--area" placeholder="Write a comment" onClick={handleClick} />
+                <textarea
+                    className="post__form--area"
+                    placeholder="Write a comment"
+                    onChange={handleChange}
+                    onClick={handleClick}
+                />
                 {showCommentButton &&
-                    <button className="post__form--button">Comment</button>
+                    <button
+                        className={`post__form--button ${commentButtonDisabled ? 'disabled' : 'active'}`}
+                        onClick={handleComment}
+                        disabled={commentButtonDisabled}
+                    >
+                        Comment
+                    </button>
                 }
             </form>
         </article>
