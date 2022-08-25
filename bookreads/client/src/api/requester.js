9c744bc52b1f6631ce7f9837e1b63ad8ab852caf
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-25 18:01:49 +0300

    Implement create comment request function

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -23,7 +23,7 @@ export const deleteBook = (bookId) => API.delete(`/books/${bookId}`);
 export const getPosts = () => API.get('/');
 export const createPost = (postData) => API.post('/', postData);
 export const likePost = (postId, userId, userName) => API.post(`/posts/${postId}`, { userId, userName });
-
+export const createComment = (postId, commentData) => API.post(`/post/${postId}/comment`, commentData);
 export const getUserById = (userId) => API.get(`/user/${userId}`);
 
 export const signin = (userData) => API.post(`/user/signin`, userData);
