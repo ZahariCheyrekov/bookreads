38951b58d914ced2291f012e11858ff1e0eaf5dd
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-26 16:02:15 +0300

    Import post likes component

--- bookreads/client/src/App.js
+++ bookreads/client/src/App.js
@@ -9,6 +9,7 @@ import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
 import Details from './components/Books/Details/Details';
 import Review from './components/Books/Reviews/Review/Review';
+import PostLikes from './components/Home/Posts/Post/PostLikes/PostLikes';
 import BookForm from './components/Books/BookForm/BookForm';
 import Auth from './components/Auth/Auth';
 import NotFound from './components/NotFound/NotFound';
@@ -27,6 +28,7 @@ function App() {
                     <Route path="/user/:name/:id" element={<Profile />} />
                     <Route path='/books/:id' element={<Details />} />
                     <Route path='/review/edit/:id' element={<Review />} />
+                    <Route path='/post/:id/likes' element={<PostLikes />} />
 
                     {['create', 'books/:id/edit'].map((path) => (
                         <Route key={path} path={`/${path}`} element={<BookForm />} />
