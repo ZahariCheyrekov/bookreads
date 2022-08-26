54f8eb1180d2be997e5543ce55935b3c6f0691d8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-26 16:02:33 +0300

    Create get post likes api request

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -22,6 +22,7 @@ export const deleteBook = (bookId) => API.delete(`/books/${bookId}`);
 
 export const getPosts = () => API.get('/');
 export const getComments = (postId) => API.get(`/post/${postId}/comments`);
+export const getPostLikes = (postId) => API.get(`/post/${postId}/likes`);
 export const createPost = (postData) => API.post('/', postData);
 export const likePost = (postId, userId, userName) => API.post(`/posts/${postId}`, { userId, userName });
 export const createComment = (postId, commentData) => API.post(`/post/${postId}/comment`, commentData);
