17ba2f32205bc49eff5ce5eb40b4c17bde27d72c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-27 23:04:31 +0300

    Create book component structure

--- bookreads/client/src/components/Books/Details/Recommended/Book/Book.js
+++ bookreads/client/src/components/Books/Details/Recommended/Book/Book.js
@@ -1,9 +1,19 @@
 const Book = ({ book }) => {
-    console.log(book);
-    
     return (
-        <li>
-            book
+        <li className="book">
+            <article className="book__article">
+                <img
+                    src={book.bookCoverUrl}
+                    alt={book.title}
+                    className="book__article--img"
+                />
+            </article>
+            <h4 className="title__name">
+                {book.title}
+            </h4>
+            <h5 className="book__author--name">
+                {book.author}
+            </h5>
         </li>
     );
 }
