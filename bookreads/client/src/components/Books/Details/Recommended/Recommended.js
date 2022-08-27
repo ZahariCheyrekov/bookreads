9c938ed0358d8265a74b1401a725a3c2b2c34db6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-27 18:41:23 +0300

    Implement recommended section useEffect

--- bookreads/client/src/components/Books/Details/Recommended/Recommended.js
+++ bookreads/client/src/components/Books/Details/Recommended/Recommended.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getBooks } from '../../../../services/book';
+
 import './Recommended.css';
 
-const Recommended = () => {
+const Recommended = ({ tags }) => {
+    const { id } = useParams();
+    const [recommendedBooks, setRecommendedBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchRecommendedBooks = async () => {
+            const books = await getBooks(id);
+            setRecommendedBooks(books);
+        }
+        fetchRecommendedBooks();
+    }, [id, tags]);
+
     return (
         <section className="recommended">
             <h3 className="recommended__title">Recommended books</h3>
+
+
             <hr className="hr__divider" />
         </section>
     );
