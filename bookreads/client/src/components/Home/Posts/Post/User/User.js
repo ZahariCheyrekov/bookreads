151c871b0f9a912936950e0e56d77514dbc22b62
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-27 13:54:18 +0300

    Fix user img source

--- bookreads/client/src/components/Home/Posts/Post/User/User.js
+++ bookreads/client/src/components/Home/Posts/Post/User/User.js
@@ -1,19 +1,20 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import defaultUserPhoto from '../../../../../assets/default-user-photo.png';
+import { AuthContext } from '../../../../../contexts/AuthContext';
 
 import './User.css';
 
-const User = ({ user, creatorId, abs }) => {
+const User = ({ abs }) => {
+    const { user } = useContext(AuthContext);
 
     return (
-        <Link to={`/user/${user?.split(' ').join('').toLowerCase()}/${creatorId}`}>
+        <Link to={`/user/${user?.result?.name?.split(' ').join('').toLowerCase()}/${user?.result?._id}`}>
             <article className={`post__user ${abs && 'abs'}`}>
                 <img className="user__img"
                     src={
-                        user?.result?.imageUrl
-                            ? user?.result?.imageUrl
-                            : defaultUserPhoto
+                        user?.result?.imageUrl ? user?.result?.imageUrl : defaultUserPhoto
                     }
                     alt={`${user?.result?.name}`}
                 />
