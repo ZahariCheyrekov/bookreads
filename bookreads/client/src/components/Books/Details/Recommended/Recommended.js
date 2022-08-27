ed26aa1a83c0638a3dbf08881396e42e6f328416
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-27 22:55:04 +0300

    Fix book component properties

--- bookreads/client/src/components/Books/Details/Recommended/Recommended.js
+++ bookreads/client/src/components/Books/Details/Recommended/Recommended.js
@@ -23,7 +23,7 @@ const Recommended = ({ tags }) => {
             <h3 className="recommended__title">Recommended books</h3>
             <ul className="recommended__books">
                 {recommendedBooks.map(book =>
-                    <Book />
+                    <Book book={book} />
                 )}
             </ul>
             <hr className="hr__divider" />
