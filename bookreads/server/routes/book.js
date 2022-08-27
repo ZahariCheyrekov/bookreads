256187d1d65ab983ff993c44973c7e29da144f3e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-27 18:42:46 +0300

    Import get books by tags and create route for it

--- bookreads/server/routes/book.js
+++ bookreads/server/routes/book.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getBookById, createBook, editBook, deleteBook } from '../controllers/book.js';
+import { getBookById, getBooksByTags, createBook, editBook, deleteBook } from '../controllers/book.js';
 
 const router = express.Router();
 
 router.get('/:id', getBookById);
+router.get('/:id/recommended', getBooksByTags);
 router.post('/', createBook);
 router.patch('/:id/edit', editBook);
 router.delete('/:id', deleteBook);
