65cbcd380fa7e9f542918e543d8a607f342d00a2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 12:26:45 +0300

    Fix create review controller function

--- bookreads/server/controllers/review.js
+++ bookreads/server/controllers/review.js
@@ -1,8 +1,22 @@
 import mongoose from 'mongoose';
 
-export const createReview = (req, res) => {
+import ReviewSchema from '../models/Review.js';
+
+export const createReview = async (req, res) => {
     const { id } = req.params;
     const reviewData = req.body;
 
-    console.log(id, reviewData);
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`Unable to find book with id: ${id}`);
+    }
+
+    const review = await ReviewSchema(reviewData);
+
+    try {
+        await review.save();
+        res.status(201).json(review);
+
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
