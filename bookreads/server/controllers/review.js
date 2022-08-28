80a68f0b50b63d07224ace5fa2c2dc385edd4b97
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 22:34:26 +0300

    Create comment on review controller function

--- bookreads/server/controllers/review.js
+++ bookreads/server/controllers/review.js
@@ -57,3 +57,15 @@ export const likeReview = async (req, res) => {
 
     return res.status(200).json({ userLike: like });
 }
+
+export const addCommentOnReview = async (req, res) => {
+    const { id } = req.params;
+    const commentData = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`Unable to find book with id: ${id}`);
+    }
+
+    const review = await ReviewSchema.findById(id);
+    console.log(review);
+}
