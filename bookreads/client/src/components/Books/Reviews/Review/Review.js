c769a8d4bcd7e1a17f7b35ff21eb2ffa0618e11c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 17:07:33 +0300

    Fix review component content

--- bookreads/client/src/components/Books/Reviews/Review/Review.js
+++ bookreads/client/src/components/Books/Reviews/Review/Review.js
@@ -1,21 +1,55 @@
+import { useState } from 'react';
+import './Review.css';
+
 const Review = ({ review }) => {
+    const [showContent, setShowContent] = useState(false);
     console.log(review);
 
+    const handleShowContent = () => {
+        setShowContent(prevState => !prevState);
+    }
+
     return (
-        <li
-            className="review__item"
-        >
-            <aside className="review__aside">
-                <img
-                    className="review__user--img"
-                    src={review.user.imageUrl}
-                    alt={review.user.name}
-                />
-                <h4 className="review__user--name">
-                    {review.user.name}
-                </h4>
-            </aside>
-        </li>
+        <>
+            {review
+                ? <li className="review__item">
+                    <aside className="review__aside">
+                        <img
+                            className="review__user--img"
+                            src={review.user.imageUrl}
+                            alt={review.user.name}
+                        />
+                        <h4 className="review__user--name">
+                            {review.user.name}
+                        </h4>
+                        <button className="review__button--follow">
+                            Follow
+                        </button>
+                    </aside>
+                    <article className="review__content">
+                        <section className={`review__section--content ${showContent && 'active'}`}>
+                            {review.reviewContent.map((paragraph, index) =>
+                                <p
+                                    key={index}
+                                    className="review__paragraph"
+                                >
+                                    {paragraph}
+                                </p>
+                            )}
+                            <span
+                                className={`review__span ${showContent ? 'less' : 'more'}`}
+                                onClick={handleShowContent}
+                            >
+                                Show {showContent ? 'less' : 'more'}&nbsp;
+                                <i className="fa-solid fa-angle-down down-arrow"></i>
+                            </span>
+                        </section>
+                        <hr className="hr__divider"></hr>
+                    </article>
+                </li>
+                : null
+            }
+        </>
     );
 }
 
