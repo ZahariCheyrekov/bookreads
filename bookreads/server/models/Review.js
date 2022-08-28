628fc624b231b300a01885dff32c01f9afcd3112
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 11:45:31 +0300

    Create review mongoose schema

---
+++ bookreads/server/models/Review.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema({
+    postId: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Object,
+        required: true
+    },
+    rating: {
+        type: Number
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+    reviewContent: {
+        type: Array
+    },
+    likes: {
+        type: Array,
+        default: []
+    },
+    comments: {
+        type: Array,
+        default: []
+    }
+});
+
+const ReviewSchema = mongoose.model('ReviewSchema', reviewSchema);
+export default ReviewSchema;
