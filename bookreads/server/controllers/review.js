d7e46b30821501c581e256903802b0a7a2635162
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 15:14:16 +0300

    Create get reviews by book id function controller

--- bookreads/server/controllers/review.js
+++ bookreads/server/controllers/review.js
@@ -2,6 +2,18 @@ import mongoose from 'mongoose';
 
 import ReviewSchema from '../models/Review.js';
 
+export const getReviewsById = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send(`Unable to find book with id: ${id}`);
+    }
+
+    const reviews = await ReviewSchema.find({ bookId: String(id) });
+
+    return res.status(200).json(reviews);
+}
+
 export const createReview = async (req, res) => {
     const { id } = req.params;
     const reviewData = req.body;
@@ -10,13 +22,15 @@ export const createReview = async (req, res) => {
         return res.status(404).send(`Unable to find book with id: ${id}`);
     }
 
+    reviewData.reviewContent = reviewData.reviewContent.trim().split(/\n+/);
+
     const review = await ReviewSchema(reviewData);
 
     try {
         await review.save();
-        res.status(201).json(review);
+        return res.status(201).json(review);
 
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        return res.status(409).json({ message: error.message });
     }
 }
