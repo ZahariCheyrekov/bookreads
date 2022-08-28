5af1c153383f2f5fa943e33eac9cdec7735de6ee
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 18:05:48 +0300

    Add like review function and create route for it

--- bookreads/server/routes/review.js
+++ bookreads/server/routes/review.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getReviewsById, createReview } from '../controllers/review.js';
+import { getReviewsById, createReview, likeReview } from '../controllers/review.js';
 
 const router = express.Router();
 
 router.get('/books/:id/reviews', getReviewsById);
 router.post('/review/edit/:id', createReview);
+router.post('/books/:id', likeReview);
 
 export default router;
