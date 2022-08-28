44f471ce0d86e896c950dcc82f9f44fce616ea64
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-28 22:33:37 +0300

    Import add comment review and create route for it

--- bookreads/server/routes/review.js
+++ bookreads/server/routes/review.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getReviewsById, createReview, likeReview } from '../controllers/review.js';
+import { getReviewsById, createReview, likeReview, addCommentOnReview } from '../controllers/review.js';
 
 const router = express.Router();
 
 router.get('/books/:id/reviews', getReviewsById);
 router.post('/review/edit/:id', createReview);
 router.post('/books/:id', likeReview);
+router.post('/books/:id', addCommentOnReview);
 
 export default router;
