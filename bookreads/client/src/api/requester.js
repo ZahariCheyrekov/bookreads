6a6787e30f4255409971619de149493600e66668
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-29 17:28:03 +0300

    Implement delete review comment api request

--- bookreads/client/src/api/requester.js
+++ bookreads/client/src/api/requester.js
@@ -25,6 +25,7 @@ export const getReviewsById = (bookId) => API.get(`/books/${bookId}/reviews`);
 export const createReview = (bookId, reviewData) => API.post(`/review/edit/${bookId}`, reviewData);
 export const likeReview = (reviewId, like) => API.post(`/books/${reviewId}`, { like });
 export const commentOnReview = (reviewId, commentData) => API.post(`/books/${reviewId}/comment`, commentData);
+export const deleteReviewComment = (reviewId, commentId) => API.delete(`/books/${reviewId}/comment/${commentId}`,);
 
 export const getPosts = () => API.get('/');
 export const getComments = (postId) => API.get(`/post/${postId}/comments`);
