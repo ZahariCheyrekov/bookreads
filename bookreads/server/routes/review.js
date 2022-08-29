d191a83dc0b8bc949d411d3ffcddd4b8b5eb7c21
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-29 17:26:55 +0300

    Import delete comment and create route for it

--- bookreads/server/routes/review.js
+++ bookreads/server/routes/review.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
-import { getReviewsById, createReview, likeReview, addCommentOnReview } from '../controllers/review.js';
+import {
+    getReviewsById,
+    createReview,
+    likeReview,
+    addCommentOnReview,
+    deleteCommentOnReview,
+} from '../controllers/review.js';
+
 
 const router = express.Router();
 
@@ -8,5 +15,6 @@ router.get('/books/:id/reviews', getReviewsById);
 router.post('/review/edit/:id', createReview);
 router.post('/books/:id', likeReview);
 router.post('/books/:id/comment', addCommentOnReview);
+router.delete('/books/:id/comment/:commentId', deleteCommentOnReview);
 
 export default router;
