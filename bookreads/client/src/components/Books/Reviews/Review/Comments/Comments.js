74b00f5bef8553a12b392f03e70d3e716d229bc2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-29 10:24:08 +0300

    Create comments component

---
+++ bookreads/client/src/components/Books/Reviews/Review/Comments/Comments.js
@@ -0,0 +1,42 @@
+import { useContext, useState } from 'react';
+
+import { AuthContext } from '../../../../../contexts/AuthContext';
+import { createCommentOnReview } from '../../../../../services/review';
+
+const Comments = ({ review, comments, setComments }) => {
+    const { user } = useContext(AuthContext);
+    const [comment, setComment] = useState('');
+
+    const handleComment = () => {
+        const commentData = {
+            user: review.user,
+            comment,
+            createdAt: new Date()
+        }
+        createCommentOnReview(review._id, commentData);
+    }
+    
+    const handleChange = (ev) => {
+        setComment(ev.target.value);
+    }
+
+    return (
+        <section className="review__comments">
+            <hr className="hr__divider" />
+            <section className="review__section--write">
+                <img src={user?.result?.imageUrl} alt={user?.result?.name} />
+                <form classNme="areview__form">
+                    <textarea className="review__form--textarea" onChange={handleChange} />
+                    <button
+                        className="review__form--btn"
+                        onClick={handleComment}
+                    >
+                        Add comment
+                    </button>
+                </form>
+            </section>
+        </section>
+    );
+}
+
+export default Comments;
