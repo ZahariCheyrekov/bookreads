2c359c053e7cde07c0a09b4ad9643ecd80d00cfc
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-29 22:20:50 +0300

    Create book api

---
+++ bookreads/client/src/api/bookAPI.js
@@ -0,0 +1,11 @@
+import { API } from './api';
+
+export const getBookById = (id) => API.get(`/books/${id}`);
+
+export const getBooksByTags = (bookId) => API.get(`/books/${bookId}/recommended`);
+
+export const createBook = (bookData) => API.post('/books', bookData);
+
+export const editBook = (bookId, bookData) => API.patch(`/books/${bookId}/edit`, bookData);
+
+export const deleteBook = (bookId) => API.delete(`/books/${bookId}`);
