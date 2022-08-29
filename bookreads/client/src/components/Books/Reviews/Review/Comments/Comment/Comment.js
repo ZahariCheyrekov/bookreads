a8f09afea91c32bb34db97ca7e4a316551d3b22f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-29 12:26:52 +0300

    Fix comment component

--- bookreads/client/src/components/Books/Reviews/Review/Comments/Comment/Comment.js
+++ bookreads/client/src/components/Books/Reviews/Review/Comments/Comment/Comment.js
@@ -1,3 +1,4 @@
+import uuid from 'react-uuid';
 import { Link } from 'react-router-dom';
 
 import './Comment.css';
@@ -5,7 +6,7 @@ import './Comment.css';
 const Comment = ({ comment }) => {
     return (
         <li className="review__user--comment">
-            <article className="review__user__comment--img">
+            < article className="review__user__comment--img" >
                 <Link to={`/user/${comment.user.name.split(' ').join('-').toLowerCase()}/${comment.user.id}`}>
                     <img
                         className="review__user__comment--img"
@@ -13,22 +14,25 @@ const Comment = ({ comment }) => {
                         alt={comment.user.name}
                     />
                 </Link>
-            </article>
+            </article >
             <article className="review__user__comment--content">
-                <Link to={`/user/${comment.user.name.split(' ').join('-').toLowerCase()}/${comment.user.id}`}>
-                    <h4 className="review__user__comment--name">
+                <h4 className="review__user__comment--name">
+                    <Link to={`/user/${comment.user.name.split(' ').join('-').toLowerCase()}/${comment.user.id}`}>
                         {comment.user.name}
-                    </h4>
-                </Link>
+                    </Link>
+                </h4>
                 <summary className="review__user__comment--summray">
                     {comment.comment.map(paragraph =>
-                        <p className="review__comment__summary--paragraph">
+                        <p
+                            key={uuid()}
+                            className="review__comment__summary--paragraph"
+                        >
                             {paragraph}
                         </p>
                     )}
                 </summary>
             </article>
-        </li>
+        </li >
     );
 }
 
