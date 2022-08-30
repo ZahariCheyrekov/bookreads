e1cf4d1b04d037d887bd7397b2fe6a4ae7670269
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-30 15:18:43 +0300

    Add recommended arrows

--- bookreads/client/src/components/Books/Details/Recommended/Recommended.js
+++ bookreads/client/src/components/Books/Details/Recommended/Recommended.js
@@ -20,7 +20,13 @@ const Recommended = () => {
 
     return (
         <section className="recommended">
-            <h3 className="recommended__title">Recommended books</h3>
+            <article className="recommended__artile--top">
+                <h3 className="recommended__title">Recommended books</h3>
+                <span className="recommended__arrows">
+                    <i class="fa-solid fa-chevron-left"></i>
+                    <i class="fa-solid fa-chevron-right"></i>
+                </span>
+            </article>
             <ul className="recommended__books">
                 {recommendedBooks.map(book =>
                     <Book
