0cc5908405831a84feec1b22098863d1ce11457c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-30 23:13:46 +0300

    Import get user link function and use it

--- bookreads/client/src/components/Navigation/Menu/Menu.js
+++ bookreads/client/src/components/Navigation/Menu/Menu.js
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AuthContext } from '../../../contexts/AuthContext';
+import { getUserLink } from '../../../utils/getUserLink';
 
 import './Menu.css';
 
@@ -12,15 +13,13 @@ const Menu = ({ profileOpen, handleProfileClick, logout }) => {
         <>
             {profileOpen && (
                 <ul className="header__article--ul article__profile--ul">
-                    <Link to={`/user/${user?.result?.name.split(' ').join('')
-                        .toLowerCase()}/${user?.result?.googleId || user?.result?._id}`}
+                    <Link to={getUserLink(user.result.name, user.result._id)}
                         onClick={handleProfileClick}
                     >
                         <h4 className="profile__ul--name">{user?.result?.name}</h4>
                     </Link>
                     <li className="profile__ul--li">
-                        <Link to={`/user/${user?.result?.name.split(' ').join('')
-                            .toLowerCase()}/${user?.result?.googleId || user?.result?._id}`}
+                        <Link to={getUserLink(user.result.name, user.result._id)}
                             onClick={handleProfileClick}
                         >
                             Profile
