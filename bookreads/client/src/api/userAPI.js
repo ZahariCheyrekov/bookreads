fe85a85f52c74d8f9cfb6adfd17e96fea9102f4f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 18:45:46 +0300

    Create get posts by user id function

--- bookreads/client/src/api/userAPI.js
+++ bookreads/client/src/api/userAPI.js
@@ -2,6 +2,8 @@ import { API } from './api';
 
 export const getUserById = (userId) => API.get(`/user/${userId}`);
 
+export const getPostsByUserId = (name, userId) => API.get(`/user/${name}/${userId}`);
+
 export const uploadUserImage = (userId, imageUrl) => API.patch(`/user/${userId}/image`, { imageUrl });
 
 export const signin = (userData) => API.post(`/user/signin`, userData);
