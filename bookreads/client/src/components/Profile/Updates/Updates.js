895f882f3e9f574c1e75f240bedcbcd8e5d1612d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 21:36:03 +0300

    Add hr tag

--- bookreads/client/src/components/Profile/Updates/Updates.js
+++ bookreads/client/src/components/Profile/Updates/Updates.js
@@ -24,6 +24,7 @@ const Updates = ({ userId, currentUser }) => {
                     <h5 className="profile__updates--title">
                         {currentUser.name.split(' ')[0]}'s recent updates
                     </h5>
+                    <hr className="profile__hr" />
                     <Posts userPosts={userPosts} />
                 </section>
             }
