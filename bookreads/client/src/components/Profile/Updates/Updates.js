4951ffb35ce68015222526199ca86f88c514e467
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 18:46:08 +0300

    Create use effect hook add set user posts

--- bookreads/client/src/components/Profile/Updates/Updates.js
+++ bookreads/client/src/components/Profile/Updates/Updates.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import { getUserPostsById } from '../../../services/user';
+import Posts from '../../Home/Posts/Posts';
+
 import './Updates.css';
 
-const Updates = () => {
+const Updates = ({ userId, currentUser }) => {
+    const [userPosts, setUserPosts] = useState([]);
+
+    useEffect(() => {
+        const getUserPosts = async () => {
+            const posts = await getUserPostsById(currentUser.name.split(' ').join('-').toLowerCase(), userId);
+            setUserPosts(posts);
+        }
+        getUserPosts();
+    }, [userId, currentUser?.name]);
+
+    console.log(userPosts);
     return (
-        <section className="profile__updates">
-            <h5 className="profile__updates--title">
-                {currentUser.name.split(' ')[0]}'s recent updates
-            </h5>
-        </section>
+        <>
+            {currentUser &&
+                <section className="profile__updates">
+                    <h5 className="profile__updates--title">
+                        {currentUser.name.split(' ')[0]}'s recent updates
+                    </h5>
+                    <Posts userPosts={userPosts} />
+                </section>
+            }
+        </>
     );
 }
 
