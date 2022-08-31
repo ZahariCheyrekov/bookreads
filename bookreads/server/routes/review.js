6c64875512ded8dedecbf517d514acfa74faad7d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 22:57:00 +0300

    Import get user rating function

--- bookreads/server/routes/review.js
+++ bookreads/server/routes/review.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import {
+    getUserRating,
     getReviewsById,
     createReview,
     likeReview,
@@ -12,6 +13,7 @@ import {
 const router = express.Router();
 
 router.get('/books/:id/reviews', getReviewsById);
+router.get('/books/:id/:userId', getUserRating);
 router.post('/review/edit/:id', createReview);
 router.post('/books/:id', likeReview);
 router.post('/books/:id/comment', addCommentOnReview);
