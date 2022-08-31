78bc4a7eba4b6169d602c7ef8e40e44ca9fb3059
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 18:46:27 +0300

    Import get user posts by id and add route for it

--- bookreads/server/routes/user.js
+++ bookreads/server/routes/user.js
@@ -1,11 +1,19 @@
 import express from 'express';
 
-import { getUserById, getUsers, signin, signup, uploadUserImage } from '../controllers/user.js';
+import {
+    getUserById,
+    getUsers,
+    getUserPostsById,
+    signin,
+    signup,
+    uploadUserImage
+} from '../controllers/user.js';
 
 const router = express.Router();
 
 router.get('/:id', getUserById);
 router.get('/', getUsers);
+router.get('/:name/:id', getUserPostsById);
 router.post('/signin', signin);
 router.post('/signup', signup);
 router.patch('/:id/image', uploadUserImage);
