db16799fcb4cd65eb51c1fc25f52bd1abaf19389
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-08-31 12:35:40 +0300

    Fix comment section

--- bookreads/client/src/components/Home/Posts/Post/CommentSection/CommentSection.js
+++ bookreads/client/src/components/Home/Posts/Post/CommentSection/CommentSection.js
@@ -1,35 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import uuid from 'react-uuid';
 
 import Comment from './Comment/Comment';
 import CommentForm from './CommentForm/CommentForm';
 
-import { getComments } from '../../../../../services/post';
-
 import './CommentSection.css';
 
-const CommentSection = ({ postId, postCreatorId }) => {
-    const [comments, setComments] = useState([]);
-
-    useEffect(() => {
-        const fetchComments = async () => {
-            const comments = await getComments(postId);
-            setComments(comments);
-        }
-        fetchComments();
-    }, [postId]);
+const CommentSection = ({ postId, postCreatorId, comments }) => {
+    const [currentComments, setCurrentComments] = useState(comments);
 
     return (
         <>
             {comments?.length > 0 &&
                 < section className="comment__section">
                     <ul className="comment__ul">
-                        {comments.map(comment =>
+                        {currentComments.map(comment =>
                             <Comment
                                 key={uuid()}
                                 comment={comment}
-                                comments={comments}
-                                setComments={setComments}
+                                currentComments={currentComments}
+                                setCurrentComments={setCurrentComments}
                                 postId={postId}
                                 postCreatorId={postCreatorId}
                             />
@@ -40,8 +30,8 @@ const CommentSection = ({ postId, postCreatorId }) => {
 
             <CommentForm
                 postId={postId}
-                comments={comments}
-                setComments={setComments}
+                currentComments={currentComments}
+                setCurrentComments={setCurrentComments}
             />
         </>
     );
