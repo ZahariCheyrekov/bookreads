1dae2ebf4bf2f1c8948a6f5092d9e02756a217e4
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-01 21:35:28 +0300

    Add rated a book and reviewd a book action types

--- bookreads/client/src/constants/actionType.js
+++ bookreads/client/src/constants/actionType.js
@@ -2,6 +2,11 @@ export const SIGN_IN = 'signin';
 
 export const SIGN_UP = 'signup';
 
+
 export const CREATED_A_BOOK = 'created a book';
 
 export const EDITED_A_BOOK = 'edited a book';
+
+export const RATED_A_BOOK = 'rated';
+
+export const REVIEWED_A_BOOK = 'reviewed';
