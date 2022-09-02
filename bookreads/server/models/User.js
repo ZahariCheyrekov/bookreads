eae345a03cb4a837b522110e4b872056e2114ebb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-02 10:38:36 +0300

    Add book shelves to user schema

--- bookreads/server/models/User.js
+++ bookreads/server/models/User.js
@@ -15,6 +15,11 @@ const userSchema = mongoose.Schema({
     },
     imageUrl: {
         type: String
+    },
+    shelves: {
+        read: [],
+        currentlyReading: [],
+        toRead: []
     }
 });
 
