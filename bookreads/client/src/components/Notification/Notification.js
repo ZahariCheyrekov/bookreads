d42da9bd5e6aaa29f87e62d3c19938ffc6bc2f07
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-05 16:31:22 +0300

    Fix notification component logic

--- bookreads/client/src/components/Notification/Notification.js
+++ bookreads/client/src/components/Notification/Notification.js
@@ -1,10 +1,29 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { NotificationContext } from '../../contexts/NotificationContext';
+
 import './Notification.css';
 
 const Notification = () => {
+    const { notificationMessage } = useContext(NotificationContext);
+    const [isVisible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true);
+
+        setInterval(() => {
+            setVisible(false);
+        }, 5000);
+    }, [notificationMessage]);
+
     return (
-        <section className="notification active">
-            Notification;
-        </section>
+        <>
+            {notificationMessage !== '' &&
+                <section className={`notification active ${isVisible && 'active'}`}>
+                    {notificationMessage}
+                </section >
+            }
+        </>
     );
 }
 
