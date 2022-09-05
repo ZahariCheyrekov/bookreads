bae35e1c45017648ea498e50494f01b1954eb800
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-05 15:57:22 +0300

    Create notification component

---
+++ bookreads/client/src/components/Notification/Notification.js
@@ -0,0 +1,11 @@
+import './Notification.css';
+
+const Notification = () => {
+    return (
+        <section className="notification active">
+            Notification;
+        </section>
+    );
+}
+
+export default Notification;
