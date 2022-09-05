36f4629a0dece61242e032fd84bfb28de426c9a5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-05 22:48:52 +0300

    Import delete post by id and add route for it

--- bookreads/server/routes/post.js
+++ bookreads/server/routes/post.js
@@ -7,7 +7,8 @@ import {
     createComment,
     getComments,
     getPostLikes,
-    deleteComment
+    deleteComment,
+    deletePostById
 } from '../controllers/post.js';
 
 
@@ -20,5 +21,6 @@ router.get('/post/:id/comments', getComments);
 router.post('/post/:id/comment', createComment);
 router.get('/post/:id/likes', getPostLikes);
 router.delete('/post/:id/comment/:commentId', deleteComment);
+router.delete('/post/:id', deletePostById);
 
 export default router;
