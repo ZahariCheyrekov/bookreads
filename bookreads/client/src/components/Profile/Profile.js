9a4d221ff70d09c4a52d4c7e8113ccbd529b6115
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-06 12:22:31 +0300

    Fix profile component

--- bookreads/client/src/components/Profile/Profile.js
+++ bookreads/client/src/components/Profile/Profile.js
@@ -1,21 +1,18 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-// import FileBase64 from 'react-file-base64';
 
 import defaultUserPhoto from '../../assets/default-user-photo.png';
 
 import { getUserById } from '../../services/user';
-// import { uploadUserImage } from '../../api/userAPI';
 
 import Updates from './Updates/Updates';
-import Spinner from '../Spinner/Spinner';
+import Spinner from '../common/Spinner/Spinner';
 
 import './Profile.css';
 
 const Profile = () => {
     const { id } = useParams();
     const [currentUser, setCurrentUser] = useState(null);
-    // const [image, setImage] = useState('');
 
     useEffect(() => {
         const fetchUser = async () => {
@@ -24,11 +21,6 @@ const Profile = () => {
         }
         fetchUser();
     }, [id]);
-
-    // const uploadImage = () => {
-    //     const imageUrl = image.base64;
-    //     uploadUserImage(user?.result?._id, imageUrl);
-    // }
 
     return (
         <main className="main__profile">
@@ -43,23 +35,6 @@ const Profile = () => {
                         <h4 className="profile__section--user">
                             {currentUser?.name}
                         </h4>
-                        {/* <Link to={'/user/edit'}>
-                    <button className="profile__section--edit">
-                        Edit profile
-                    </button>
-                </Link> */}
-                        {/* <FileBase64
-                    type="file"
-                    multiple={false}
-                    onDone={(base64) => (setImage(base64))} />
-                {image &&
-                    <>
-                        <img src={image.base64} alt={currentUser} />
-                        <button onClick={uploadImage}>
-                            Upload
-                        </button>
-                    </>
-                } */}
                     </section>
                     <section className="profile__user--bookshelves">
                         <h5 className="profile__bookshelves--title">
