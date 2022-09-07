f571ed7787b852d6c36d33140c36e62d2c14ba73
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-07 16:04:34 +0300

    Fetch default books for genres page

--- bookreads/client/src/pages/Genres/Genres.js
+++ bookreads/client/src/pages/Genres/Genres.js
@@ -1,16 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import { genres } from '../../constants/shelves';
+import { getDefaultBooks, getBooksByGivenGenre } from '../../services/genre';
 
 import './Genres.css';
 
 const Genres = () => {
     const [genreInput, setGenreInput] = useState('');
+    const [defaultBooks, setDefaultBooks] = useState({});
 
-    const handleFindGenre = () => {
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const books = await getDefaultBooks();
+            setDefaultBooks(books);
+        }
+        fetchBooks();
+    }, []);
 
+    const handleFindGenre = async (ev) => {
+        ev.preventDefault();
+
+        const books = await getBooksByGivenGenre(genreInput);
     }
 
     return (
@@ -33,27 +45,20 @@ const Genres = () => {
                             Find genre
                         </button>
                     </form>
-
-                    {/* TODO: Add books by some genre */}
                     <article className="genres__default--sections">
                         <section className="genres__default__section genres__default--art">
                             <h4 className="genres__section--title">
-
+                                Biography
                             </h4>
                         </section>
                         <section className="genres__default__section genres__default--biography">
                             <h4 className="genres__section--title">
-
+                                Fiction
                             </h4>
                         </section>
                         <section className="genres__default__section genres__default--classics">
                             <h4 className="genres__section--title">
-
-                            </h4>
-                        </section>
-                        <section className="genres__default__section genres__default--nonfiction">
-                            <h4 className="genres__section--title">
-
+                                Philosophy
                             </h4>
                         </section>
                     </article>
