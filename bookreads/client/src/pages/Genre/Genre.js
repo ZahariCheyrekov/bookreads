6a9b0758e560a73156eb3efb1746b9f9ce02134c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-07 18:43:10 +0300

    Create genre page

---
+++ bookreads/client/src/pages/Genre/Genre.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import { Link, useParams } from 'react-router-dom';
+
+import { getBooksByGivenGenre } from '../../services/genre';
+
+import './Genre.css';
+
+export const Genre = () => {
+    const { genre } = useParams();
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const books = await getBooksByGivenGenre(genre);
+            setBooks(books);
+        }
+        fetchBooks();
+    }, [genre]);
+
+    return (
+        <main className="main">
+            <div className="genre__books--wrapper">
+                <h3 className="genres__title">
+                    {genre.toUpperCase()}
+                </h3>
+
+                <section className="genre__books">
+                    <h4 className="genre__name--title">
+                        {genre.toUpperCase()} BOOKS
+                    </h4>
+                    <ul className="genre__books--list">
+                        {books.map(book =>
+                            <li
+                                key={book._id}
+                                className="genre__book--item"
+                            >
+                                <Link to={`/books/${book._id}`}>
+                                    <img src={book.bookCoverUrl} alt={book.title} />
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
+                </section>
+            </div>
+        </main>
+    );
+}
+
+export default Genre;
