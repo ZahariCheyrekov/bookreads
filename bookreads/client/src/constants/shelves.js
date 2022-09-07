97fcfe56ba06db6391a8e4334b58499a98e85526
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-07 15:14:49 +0300

    Add genres array constant

--- bookreads/client/src/constants/shelves.js
+++ bookreads/client/src/constants/shelves.js
@@ -3,3 +3,32 @@ export const WANT_TO_READ_SHELVE = 'toRead';
 export const CURRENTLY_READING_SHELVE = 'currentlyReading';
 
 export const READ_SHELVE = 'read';
+
+export const genres = [
+    'Art',
+    'Biography',
+    'Business',
+    'Classics',
+    'Comics',
+    'Cookbooks',
+    'Crime',
+    'Ebooks',
+    'Fantasy',
+    'Fiction',
+    'History',
+    'Horror',
+    'Comedy',
+    'Memoir',
+    'Music',
+    'Mystery',
+    'Nonfiction',
+    'Philosophy',
+    'Poetry',
+    'Psychology',
+    'Romance',
+    'Science',
+    'Science',
+    'Sports',
+    'Thriller',
+    'Travel'
+]
