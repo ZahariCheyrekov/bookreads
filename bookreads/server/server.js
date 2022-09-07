1c654eba6d97024e2370c1be16ca820537c8a387
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-07 16:06:11 +0300

    Import and use genres router

--- bookreads/server/server.js
+++ bookreads/server/server.js
@@ -6,11 +6,11 @@ import bodyParser from 'body-parser';
 import { configDatabase } from './config/configDatabase.js';
 import { DEFAULT_PORT } from './constants/index.js';
 
-import userRoutes from './routes/user.js';
-import bookRoutes from './routes/book.js';
-import postRoutes from './routes/post.js';
+import userRouter from './routes/user.js';
+import bookRouter from './routes/book.js';
+import postRouter from './routes/post.js';
 import reviewRouter from './routes/review.js';
-import genresRoutes from './routes/genre.js';
+import genresRouter from './routes/genre.js';
 
 const app = express();
 dotenv.config();
@@ -20,11 +20,11 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/user', userRoutes);
-app.use('/books', bookRoutes);
-app.use('/', postRoutes);
+app.use('/user', userRouter);
+app.use('/books', bookRouter);
+app.use('/', postRouter);
 app.use('/', reviewRouter);
-app.use('/genres', genresRoutes);
+app.use('/genres', genresRouter);
 
 app.get('/', (req, res) => {
     res.send('Application is running correctly.');
