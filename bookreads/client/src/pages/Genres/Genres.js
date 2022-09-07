e4d090ce340d30291bd289e746d7ee41b75e1ca2
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-07 13:05:09 +0300

    Create genres component form

--- bookreads/client/src/pages/Genres/Genres.js
+++ bookreads/client/src/pages/Genres/Genres.js
@@ -1,7 +1,34 @@
+import './Genres.css';
+
 const Genres = () => {
+
+    const handleFindGenre = () => {
+
+    }
+
     return (
         <main className="main">
+            <article className="genres__page--article">
+                <section className="genres">
+                    <h3 className="genres__title">
+                        Genres
+                    </h3>
+                    <form className="genres__form">
+                        <input className="genres__form--input" placeholder="Find a genre by name" />
+                        <button
+                            className="genres__form--button"
+                            onClick={handleFindGenre}
+                        >
+                            Find genre
+                        </button>
+                    </form>
 
+                    {/* TODO: Add books by some genre */}
+                </section>
+                <aside className="genres__aside">
+
+                </aside>
+            </article>
         </main>
     );
 }
