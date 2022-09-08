5dc0e9f17448f29c8331545b47408079f56a82af
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-08 10:21:50 +0300

    Extract shelves constant

--- bookreads/client/src/constants/shelves.js
+++ bookreads/client/src/constants/shelves.js
@@ -3,32 +3,3 @@ export const WANT_TO_READ_SHELVE = 'toRead';
 export const CURRENTLY_READING_SHELVE = 'currentlyReading';
 
 export const READ_SHELVE = 'read';
-
-export const genres = [
-    'Art',
-    'Biography',
-    'Business',
-    'Classics',
-    'Comics',
-    'Cookbooks',
-    'Crime',
-    'Ebooks',
-    'Fantasy',
-    'Fiction',
-    'History',
-    'Horror',
-    'Comedy',
-    'Memoir',
-    'Music',
-    'Mystery',
-    'Nonfiction',
-    'Philosophy',
-    'Poetry',
-    'Psychology',
-    'Romance',
-    'Science',
-    'Science Fiction',
-    'Sports',
-    'Thriller',
-    'Travel'
-]
