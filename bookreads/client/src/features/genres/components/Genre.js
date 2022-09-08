84f939e70bbe7d4e311e2a42f78443540a258f3f
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-08 17:21:46 +0300

    Fix genre component conditional rendering

--- bookreads/client/src/features/genres/components/Genre.js
+++ bookreads/client/src/features/genres/components/Genre.js
@@ -23,24 +23,35 @@ export const Genre = () => {
         <main className="main">
             <div className="genre__books--wrapper">
                 <section className="genre__books">
-                    <h3 className="genres__title">
-                        {genre.toUpperCase()}
-                    </h3>
                     <h4 className="genre__name--title">
                         {genre.toUpperCase()} BOOKS
                     </h4>
-                    <ul className="genre__books--list">
-                        {books.map(book =>
-                            <li
-                                key={book._id}
-                                className="genre__book--item"
-                            >
-                                <Link to={`/books/${book._id}`}>
-                                    <img src={book.bookCoverUrl} alt={book.title} />
-                                </Link>
-                            </li>
-                        )}
-                    </ul>
+                    {books.length > 0 ?
+                        <ul className="genre__books--list">
+                            {books.map(book =>
+                                <li
+                                    key={book._id}
+                                    className="genre__book--item"
+                                >
+                                    <Link to={`/books/${book._id}`}>
+                                        <img src={book.bookCoverUrl} alt={book.title} />
+                                    </Link>
+                                </li>
+                            )}
+                        </ul>
+                        :
+                        <section className="genre__section--empty">
+                            <h4 className="genre__section__title--big">
+                                No books with genre "{`${genre}`}"
+                            </h4>
+                            <h5 className="genre__section__title--small">
+                                Create book with this genre or with other one.
+                            </h5>
+                            <Link to={'/create'} className="genre__section--link">
+                                Create book
+                            </Link>
+                        </section>
+                    }
                 </section>
                 <GenreList />
             </div>
