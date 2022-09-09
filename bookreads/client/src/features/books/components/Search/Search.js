e31e17025ae886b71885435066b8f55b7b0f353e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-09 22:56:21 +0300

    Add search component wrapper

--- bookreads/client/src/features/books/components/Search/Search.js
+++ bookreads/client/src/features/books/components/Search/Search.js
@@ -3,7 +3,9 @@ import './Search.css';
 const Search = () => {
     return (
         <main className="main">
+            <div className="search__wrapper">
 
+            </div>
         </main>
     );
 }
