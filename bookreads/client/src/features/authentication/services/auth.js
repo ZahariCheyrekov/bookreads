666689fad5cb6ba63c7764e66389a585ca9d86f3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-11 19:56:24 +0300

    Import validate input function

--- bookreads/client/src/features/authentication/services/auth.js
+++ bookreads/client/src/features/authentication/services/auth.js
@@ -1,22 +1,30 @@
 import * as userAPI from '../../../api/userAPI';
 
 import { saveUser } from '../../../services/localStorage';
+import { validateInput } from '../../../validation/validateInput';
 import { SIGN_IN, SIGN_UP } from '../constants/actionTypes';
 
 export const auth = async (action, data) => {
-    try {
-        let result;
 
-        if (action === SIGN_IN) {
-            result = await userAPI.signin(data);
-        } else if (action === SIGN_UP) {
-            result = await userAPI.signup(data);
+    const isValidInput = validateInput(data);
+
+    if (isValidInput) {
+        try {
+            let result;
+
+            if (action === SIGN_IN) {
+                result = await userAPI.signin(data);
+            } else if (action === SIGN_UP) {
+                result = await userAPI.signup(data);
+            }
+
+            const user = result.data;
+            saveUser(user);
+
+        } catch (error) {
+            console.log(error);
         }
-
-        const user = result.data;
-        saveUser(user);
+    }else{
         
-    } catch (error) {
-        console.log(error);
     }
 }
