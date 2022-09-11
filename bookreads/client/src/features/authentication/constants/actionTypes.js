dca80136fd3ac3920227db228992f9e93220a813
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-11 20:34:39 +0300

    Create login and register constants

--- bookreads/client/src/features/authentication/constants/actionTypes.js
+++ bookreads/client/src/features/authentication/constants/actionTypes.js
@@ -1,3 +1,8 @@
 export const SIGN_IN = 'signin';
 
 export const SIGN_UP = 'signup';
+
+
+export const LOGIN_SUCCESSFUL = 'Login was successful';
+
+export const REGISTER_SUCCESSFUL = 'Registration successfully completed';
