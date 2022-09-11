c3d24130ff588d033993796f3cbdedda52abe3d6
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-11 20:14:47 +0300

    Fix error constants

--- bookreads/client/src/constants/errors.js
+++ bookreads/client/src/constants/errors.js
@@ -1,3 +1,3 @@
-export const ALL_FIELDS_ARE_REQUIRED = 'All fields are required!';
+export const ALL_FIELDS_ARE_REQUIRED = 'All fields are required';
 
-export const PASSWORDS_DONT_MATCH = 'Passwords don\'t match!';
+export const PASSWORDS_DONT_MATCH = 'Passwords don\'t match';
