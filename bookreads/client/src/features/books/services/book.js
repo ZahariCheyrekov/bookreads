37d4b86de27178c051c57f3aee987fed5e5bae86
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-11 20:43:37 +0300

    Fix create new book service function validation

--- bookreads/client/src/features/books/services/book.js
+++ bookreads/client/src/features/books/services/book.js
@@ -1,3 +1,6 @@
+import { ALL_FIELDS_ARE_REQUIRED } from '../../../constants/errors';
+import { notify } from '../../../lib/toastify';
+import { validateInputFields } from '../../../validation/validateInputFields';
 import * as bookAPI from '../api/bookAPI';
 
 export const getBook = async (id) => {
@@ -29,9 +32,22 @@ export const getBooksByTitle = async (bookTitle) => {
 
 export const createNewBook = async (bookData) => {
     try {
+        const isValidInput = validateInputFields(bookData);
+
+        if (!isValidInput) {
+            throw new Error(ALL_FIELDS_ARE_REQUIRED);
+        }
+
         const { data } = await bookAPI.createBook(bookData);
         return data;
+
     } catch (error) {
         console.log(error);
+
+        if (error.response) {
+            notify(error.response.data.message);
+        } else {
+            notify(error.message);
+        }
     }
 }
