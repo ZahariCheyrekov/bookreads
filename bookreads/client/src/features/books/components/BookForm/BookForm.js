144d626dd50a79eeef4f26a835cb0baac5f4684c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-11 18:05:34 +0300

    Remove notification context

--- bookreads/client/src/features/books/components/BookForm/BookForm.js
+++ bookreads/client/src/features/books/components/BookForm/BookForm.js
@@ -7,7 +7,6 @@ import { editBook } from '../../api/bookAPI';
 import { createPost } from '../../../../api/postAPI';
 
 import { AuthContext } from '../../../../contexts/AuthContext';
-import { NotificationContext } from '../../../../contexts/NotificationContext';
 
 import { createNewBook, getBook } from '../../services/book';
 import { CREATED_A_BOOK, EDITED_A_BOOK } from '../../../../constants/actionType';
@@ -21,7 +20,6 @@ const BookForm = () => {
     const navigate = useNavigate();
     const { id } = useParams();
     const { user } = useContext(AuthContext);
-    const { setNotificationMessage } = useContext(NotificationContext);
     const [bookData, setBookData] = useState(
         {
             title: '',
