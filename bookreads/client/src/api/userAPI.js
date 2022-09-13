8460f476074f217003310789c714dbc0cb926dc3
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-13 22:19:58 +0300

    Extract sign in and sign up api requests

--- bookreads/client/src/api/userAPI.js
+++ bookreads/client/src/api/userAPI.js
@@ -8,7 +8,3 @@ export const getPostsByUserId = (name, userId) => API.get(`/user/${name}/${userI
 export const uploadUserImage = (userId, imageUrl) => API.patch(`/user/${userId}/image`, { imageUrl });
 
 export const addBookToUserShelve = (userId, shelveName, book) => API.post(`/user/${userId}/shelves`, { shelveName, book });
-
-export const signin = (userData) => API.post(`/user/signin`, userData);
-
-export const signup = (userData) => API.post(`/user/signup`, userData);
