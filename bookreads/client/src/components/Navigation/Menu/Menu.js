944f55e5ee18cd001a3a3e0d13f8b2992588e375
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-14 11:25:27 +0300

    Fix menu component

--- bookreads/client/src/components/Navigation/Menu/Menu.js
+++ bookreads/client/src/components/Navigation/Menu/Menu.js
@@ -6,64 +6,60 @@ import { getUserLink } from '../../../utils/getUserLink';
 
 import './Menu.css';
 
-const Menu = ({ profileOpen, handleProfileClick, logout }) => {
+const Menu = ({ handleProfileClick, logout }) => {
     const { user } = useContext(AuthContext);
 
     return (
-        <>
-            {profileOpen && (
-                <ul className="header__article--ul article__profile--ul">
-                    <Link to={getUserLink(user?.result?.name, user?.result?._id)}
-                        onClick={handleProfileClick}
-                    >
-                        <h4 className="profile__ul--name">{user?.result?.name}</h4>
-                    </Link>
-                    <li className="profile__ul--li">
-                        <Link to={'/'}
-                            onClick={handleProfileClick}
-                        >
-                            Home
-                        </Link>
-                    </li>
-                    <li className="profile__ul--li">
-                        <Link to={getUserLink(user.result.name, user.result._id)}
-                            onClick={handleProfileClick}
-                        >
-                            Profile
-                        </Link>
-                    </li>
-                    <li className="profile__ul--li">
-                        <Link to={'/create'}
-                            onClick={handleProfileClick}
-                        >
-                            Create book
-                        </Link>
-                    </li>
-                    <li className="profile__ul--li">
-                        <Link to={'/genres'}
-                            onClick={handleProfileClick}
-                        >
-                            Browse
-                        </Link>
-                    </li>
-                    <li className="profile__ul--li">
-                        <Link to={`/user/${user.result._id}/shelves`}
-                            onClick={handleProfileClick}
-                        >
-                            Shelves
-                        </Link>
-                    </li>
-                    <li className="profile__ul--li">
-                        <Link to={'/'} onClick={() => {
-                            handleProfileClick();
-                            logout();
-                        }}>
-                            Sign out
-                        </Link>
-                    </li>
-                </ul>
-            )}
-        </>
+        <ul className="header__article--ul article__profile--ul">
+            <Link to={getUserLink(user?.result?.name, user?.result?._id)}
+                onClick={handleProfileClick}
+            >
+                <h4 className="profile__ul--name">{user?.result?.name}</h4>
+            </Link>
+            <li className="profile__ul--li">
+                <Link to={'/'}
+                    onClick={handleProfileClick}
+                >
+                    Home
+                </Link>
+            </li>
+            <li className="profile__ul--li">
+                <Link to={getUserLink(user.result.name, user.result._id)}
+                    onClick={handleProfileClick}
+                >
+                    Profile
+                </Link>
+            </li>
+            <li className="profile__ul--li">
+                <Link to={'/create'}
+                    onClick={handleProfileClick}
+                >
+                    Create book
+                </Link>
+            </li>
+            <li className="profile__ul--li">
+                <Link to={'/genres'}
+                    onClick={handleProfileClick}
+                >
+                    Browse
+                </Link>
+            </li>
+            <li className="profile__ul--li">
+                <Link to={`/user/${user.result._id}/shelves`}
+                    onClick={handleProfileClick}
+                >
+                    Shelves
+                </Link>
+            </li>
+            <li className="profile__ul--li">
+                <Link to={'/'} onClick={() => {
+                    handleProfileClick();
+                    logout();
+                }}>
+                    Sign out
+                </Link>
+            </li>
+        </ul>
     );
 }
 
