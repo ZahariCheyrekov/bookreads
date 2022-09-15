3f9e2d0f1636977b12066358720a8001525d8e86
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-15 12:25:06 +0300

    Extract edit user review link

--- bookreads/client/src/features/books/components/Reviews/Reviews.js
+++ bookreads/client/src/features/books/components/Reviews/Reviews.js
@@ -33,11 +33,6 @@ const Reviews = () => {
                         review={userReview}
                         userReview={true}
                     />
-                    <span className="reviews__user__review--edit">
-                        <Link to={`/review/edit/${id}`}>
-                            Edit review
-                        </Link>
-                    </span>
                 </>
                 :
                 <article className="reviews__user">
