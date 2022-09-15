2021a8a84f218a36f7bfb205fa1297fd22cd48c8
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-15 11:47:49 +0300

    Fix reviews user review section

--- bookreads/client/src/features/books/components/Reviews/Reviews.js
+++ bookreads/client/src/features/books/components/Reviews/Reviews.js
@@ -14,31 +14,49 @@ import { ReviewContext } from '../../contexts/ReviewContext';
 import './Reviews.css';
 
 const Reviews = () => {
-    const reviews = useContext(ReviewContext);
+    const { reviews, userReview } = useContext(ReviewContext);
     const { id } = useParams();
     const { user } = useContext(AuthContext);
 
     return (
         <section className="reviews">
-            <h3 className="reviews__title">Reviews & Ratings</h3>
-
-            <article className="reviews__user">
-                <Link to={getUserLink(user.result.name, user.result._id)}>
-                    <article className="profile__section--article">
-                        <img src={user?.result?.imageUrl ? user?.result?.imageUrl : defaultUserPhoto}
-                            alt={`${user?.result?.name}`}
-                        />
-                    </article>
-                </Link>
-
-                <h2 className="reviews__user--question">What do you think?</h2>
-                <section className="reviews__review--action">
-                    <Rating />
-                    <Link to={`/review/edit/${id}`}>
-                        <button className="reviews__user--button">Write a Review</button>
+            <h3 className="reviews__title">
+                Reviews & Ratings
+            </h3>
+            {userReview ?
+                <>
+                    <h4 className="reviews__user__review--title">
+                        My Review
+                    </h4>
+                    <Review
+                        key={userReview?._id}
+                        review={userReview}
+                        userReview={true}
+                    />
+                </>
+                :
+                <article className="reviews__user">
+                    <Link to={getUserLink(user.result.name, user.result._id)}>
+                        <article className="profile__section--article">
+                            <img src={user?.result?.imageUrl ? user?.result?.imageUrl : defaultUserPhoto}
+                                alt={`${user?.result?.name}`}
+                            />
+                        </article>
                     </Link>
-                </section>
-            </article>
+
+                    <h2 className="reviews__user--question">
+                        What do you think?
+                    </h2>
+                    <section className="reviews__review--action">
+                        <Rating />
+                        <Link to={`/review/edit/${id}`}>
+                            <button className="reviews__user--button">
+                                Write a Review
+                            </button>
+                        </Link>
+                    </section>
+                </article>
+            }
             <hr className="hr__divider" />
 
             <section className="reviews">
