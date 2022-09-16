d884e32e3ca30c14f0c90cf352a40f11d63b8095
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-16 10:53:13 +0300

    Fix use current user cutom hook

--- bookreads/client/src/hooks/useCurrentUser.js
+++ bookreads/client/src/hooks/useCurrentUser.js
@@ -1,19 +1,20 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+
+import { AuthContext } from '../contexts/AuthContext';
 
 import { getUserById } from '../services/user';
 
 export const useCurrentUser = () => {
-    const { id } = useParams();
+    const { user } = useContext(AuthContext);
     const [currentUser, setCurrentUser] = useState(null);
 
     useEffect(() => {
         const fetchUser = async () => {
-            const user = await getUserById(id);
-            setCurrentUser(user);
+            const currentUser = await getUserById(user?.result._id);
+            setCurrentUser(currentUser);
         }
         fetchUser();
-    }, [id]);
+    }, [user]);
 
     return currentUser;
 }
