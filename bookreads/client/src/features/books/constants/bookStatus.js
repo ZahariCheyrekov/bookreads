eb43187c90bd021e84b91b8e7141b3e3dd64ab77
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-17 10:54:31 +0300

    Add book post status

--- bookreads/client/src/features/books/constants/bookStatus.js
+++ bookreads/client/src/features/books/constants/bookStatus.js
@@ -3,3 +3,10 @@ export const WANT_TO_READ = 'Want to read';
 export const CURRENTLY_READING = 'Currently Reading';
 
 export const READ = 'Read';
+
+
+export const IS_READING = 'is currently reading';
+
+export const WANTS_TO_READ = 'wants to read';
+
+export const FINISHED_BOOK = 'finished reading';
