141a86b2911fddf9ecbfab5ab9fc13c729275277
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-17 20:47:01 +0300

    Extract add book to user shelve api request

--- bookreads/client/src/api/userAPI.js
+++ bookreads/client/src/api/userAPI.js
@@ -6,5 +6,3 @@ export const getUserById = (userId) => API.get(`/user/${userId}`);
 export const getPostsByUserId = (name, userId) => API.get(`/user/${name}/${userId}`);
 
 export const uploadUserImage = (userId, imageUrl) => API.patch(`/user/${userId}/image`, { imageUrl });
-
-export const addBookToUserShelve = (userId, shelveName, book) => API.post(`/user/${userId}/shelves`, { shelveName, book });
