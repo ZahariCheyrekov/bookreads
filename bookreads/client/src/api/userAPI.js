4f4049fbb9ea2eeffd05d5bb47f0415cc4877b2a
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-18 17:46:33 +0300

    Extract upload user image api request

--- bookreads/client/src/api/userAPI.js
+++ bookreads/client/src/api/userAPI.js
@@ -4,5 +4,3 @@ import { API } from '../lib/axios';
 export const getUserById = (userId) => API.get(`/user/${userId}`);
 
 export const getPostsByUserId = (name, userId) => API.get(`/user/${name}/${userId}`);
-
-export const uploadUserImage = (userId, imageUrl) => API.post(`/user/${userId}/image`, { imageUrl });
