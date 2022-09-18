7ff6b52d905217049fe3781402cf77281545eb5d
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-18 17:32:27 +0300

    Fix user api upload image api request

--- bookreads/client/src/api/userAPI.js
+++ bookreads/client/src/api/userAPI.js
@@ -5,4 +5,4 @@ export const getUserById = (userId) => API.get(`/user/${userId}`);
 
 export const getPostsByUserId = (name, userId) => API.get(`/user/${name}/${userId}`);
 
-export const uploadUserImage = (userId, imageUrl) => API.patch(`/user/${userId}/image`, { imageUrl });
+export const uploadUserImage = (userId, imageUrl) => API.post(`/user/${userId}/image`, { imageUrl });
