e7762712453a086b420df8b19a234e7c49bb7b77
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-18 17:46:48 +0300

    Add upload user image api request

--- bookreads/client/src/features/profile/api/profileAPI.js
+++ bookreads/client/src/features/profile/api/profileAPI.js
@@ -1,4 +1,6 @@
 import { API } from '../../../lib/axios';
 
+export const uploadUserImage = (userId, imageUrl) => API.post(`/user/${userId}/image`, { imageUrl });
+
 export const deleteBookFromShelve = (userId, shelveName, bookId) =>
     API.delete(`/user/${userId}/shelves/${shelveName}/${bookId}`);
