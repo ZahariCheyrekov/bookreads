bf99321f76dd37dbe28b3acce4dc4f7c8cb7db5e
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-18 17:48:53 +0300

    Create edit user profile component

---
+++ bookreads/client/src/features/profile/components/Edit.js
@@ -0,0 +1,48 @@
+import { useContext, useState } from 'react';
+import FileBase64 from 'react-file-base64';
+
+import { AuthContext } from '../../../contexts/AuthContext';
+
+import { uploadUserImage } from '../api/profileAPI';
+
+import Spinner from '../../../components/Spinner/Spinner';
+import defaultUserPhoto from '../../../assets/default-user-photo.png';
+
+import './Edit.css';
+
+const Edit = () => {
+    const { user } = useContext(AuthContext);
+    const [file, setFile] = useState('');
+
+    const uploadImage = () => {
+        const imageUrl = file.base64;
+        uploadUserImage(user?.result?._id, imageUrl);
+    }
+
+    return (
+        <main className="main">
+            {user ?
+                <section className="profile__edit--section" >
+                    <h3 className="profile__edit--title">
+                        Edit photo
+                    </h3>
+                    <FileBase64
+                        type="file"
+                        multiple={false}
+                        onDone={(base64) => (setFile(base64))} />
+                    {file &&
+                        <>
+                            <img src={file.base64} alt={user?.result.name} />
+                            <button onClick={uploadImage}>
+                                Upload
+                            </button>
+                        </>
+                    }
+                </section >
+                : <Spinner />
+            }
+        </main >
+    );
+}
+
+export default Edit;
