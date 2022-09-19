c52565d7523e06526604c7b9f97d85f56991a8cb
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-19 21:22:02 +0300

    Fix scroll to top component

--- bookreads/client/src/components/ScrollToTop.js
+++ bookreads/client/src/components/ScrollToTop.js
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+        window.scrollTo({ top: 0 });
     }, [pathname]);
 }
 
