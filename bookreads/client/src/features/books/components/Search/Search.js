765dc22ba4091cdd5300f1137339f9341e934db5
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-21 16:31:13 +0300

    Fix component render and add loading state

--- bookreads/client/src/features/books/components/Search/Search.js
+++ bookreads/client/src/features/books/components/Search/Search.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
-import GenreList from '../../../genres/components/GenreList';
-
 import { getBooksByTitle } from '../../services/book';
+
+import Spinner from '../../../../components/Spinner';
+import GenreList from '../../../genres/components/GenreList';
 
 import './Search.css';
 
@@ -12,12 +13,14 @@ const Search = () => {
     const { bookTitle } = useParams();
     const [books, setBooks] = useState([]);
     const [searchTitle, setSearchTitle] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         setSearchTitle(bookTitle)
         const fetchBooks = async () => {
             const books = await getBooksByTitle(bookTitle);
             setBooks(books);
+            setIsLoading(false);
         }
         fetchBooks();
     }, [bookTitle]);
@@ -51,44 +54,49 @@ const Search = () => {
                         <h4 className="search__title--book">
                             Title: {bookTitle}
                         </h4>
-                        {books.length > 0 ?
-                            <ul className="search__books__result--list">
-                                {books.map((book, index) =>
-                                    <li
-                                        key={index}
-                                        className="search__book__list--item"
-                                    >
-                                        <article className="search__book__result--item">
-                                            <Link to={`/books/${book._id}`}>
-                                                <img src={book.bookCoverUrl} alt={book.title} />
-                                            </Link>
-                                            <summary className="search__book--summary">
-                                                <Link to={`/books/${book._id}`}>
-                                                    <h3 className="book__result--title">
-                                                        {book.title}
-                                                    </h3>
-                                                </Link>
-                                                <h4 className="book__result--author">
-                                                    by {book.author}
-                                                </h4>
-                                            </summary>
-                                        </article>
-                                        <hr className="hr__search__divider" />
-                                    </li>
-                                )}
-                            </ul>
-                            :
-                            <section className="genre__section--empty">
-                                <h4 className="genre__section__title--big">
-                                    No result for books with title "{bookTitle}"
-                                </h4>
-                                <h5 className="genre__section__title--small">
-                                    Create book with this title
-                                </h5>
-                                <a className="genre__section--link" href="/create">
-                                    Create book
-                                </a>
-                            </section>
+                        {isLoading
+                            ? <Spinner />
+                            : <>
+                                {books.length > 0 ?
+                                    <ul className="search__books__result--list">
+                                        {books.map((book, index) =>
+                                            <li
+                                                key={index}
+                                                className="search__book__list--item"
+                                            >
+                                                <article className="search__book__result--item">
+                                                    <Link to={`/books/${book._id}`}>
+                                                        <img src={book.bookCoverUrl} alt={book.title} />
+                                                    </Link>
+                                                    <summary className="search__book--summary">
+                                                        <Link to={`/books/${book._id}`}>
+                                                            <h3 className="book__result--title">
+                                                                {book.title}
+                                                            </h3>
+                                                        </Link>
+                                                        <h4 className="book__result--author">
+                                                            by {book.author}
+                                                        </h4>
+                                                    </summary>
+                                                </article>
+                                                <hr className="hr__search__divider" />
+                                            </li>
+                                        )}
+                                    </ul>
+                                    :
+                                    <section className="genre__section--empty">
+                                        <h4 className="genre__section__title--big">
+                                            No result for books with title "{bookTitle}"
+                                        </h4>
+                                        <h5 className="genre__section__title--small">
+                                            Create book with this title
+                                        </h5>
+                                        <a className="genre__section--link" href="/create">
+                                            Create book
+                                        </a>
+                                    </section>
+                                }
+                            </>
                         }
                     </section>
                 </section>
