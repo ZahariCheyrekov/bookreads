e6229cf92e3c2b63602f8cd3cae42b68556d23c0
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-21 16:24:29 +0300

    Fix genre component render and add loading state

--- bookreads/client/src/features/genres/components/Genre.js
+++ bookreads/client/src/features/genres/components/Genre.js
@@ -1,20 +1,23 @@
 import { useEffect, useState } from 'react';
-
 import { Link, useParams } from 'react-router-dom';
 
-import GenreList from './GenreList';
 import { getBooksByGivenGenre } from '../services/genre';
+
+import Spinner from '../../../components/Spinner';
+import GenreList from './GenreList';
 
 import './Genre.css';
 
 export const Genre = () => {
     const { genre } = useParams();
     const [books, setBooks] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchBooks = async () => {
             const books = await getBooksByGivenGenre(genre);
             setBooks(books);
+            setIsLoading(false);
         }
         fetchBooks();
     }, [genre]);
@@ -26,31 +29,36 @@ export const Genre = () => {
                     <h4 className="genre__name--title">
                         {genre.toUpperCase()} BOOKS
                     </h4>
-                    {books.length > 0 ?
-                        <ul className="genre__books--list">
-                            {books.map(book =>
-                                <li
-                                    className="genre__book--item"
-                                    key={book._id}
-                                >
-                                    <Link to={`/books/${book._id}`}>
-                                        <img src={book.bookCoverUrl} alt={book.title} />
+                    {isLoading
+                        ? <Spinner />
+                        : <>
+                            {books.length > 0 ?
+                                <ul className="genre__books--list">
+                                    {books.map(book =>
+                                        <li
+                                            className="genre__book--item"
+                                            key={book._id}
+                                        >
+                                            <Link to={`/books/${book._id}`}>
+                                                <img src={book.bookCoverUrl} alt={book.title} />
+                                            </Link>
+                                        </li>
+                                    )}
+                                </ul>
+                                :
+                                <section className="genre__section--empty">
+                                    <h4 className="genre__section__title--big">
+                                        No books with genre "{`${genre}`}"
+                                    </h4>
+                                    <h5 className="genre__section__title--small">
+                                        Create book with this genre or with other one
+                                    </h5>
+                                    <Link to={'/create'} className="genre__section--link">
+                                        Create book
                                     </Link>
-                                </li>
-                            )}
-                        </ul>
-                        :
-                        <section className="genre__section--empty">
-                            <h4 className="genre__section__title--big">
-                                No books with genre "{`${genre}`}"
-                            </h4>
-                            <h5 className="genre__section__title--small">
-                                Create book with this genre or with other one
-                            </h5>
-                            <Link to={'/create'} className="genre__section--link">
-                                Create book
-                            </Link>
-                        </section>
+                                </section>
+                            }
+                        </>
                     }
                 </section>
                 <GenreList />
